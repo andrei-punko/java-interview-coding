662ae464d1007c0415c6a83d972f2295693b31a6
Andrei Punko <andd3dfx@gmail.com>
2022-04-08 14:23:16 +0300

    Fix typos and javadoc formatting

--- src/main/java/by/andd3dfx/string/Palindrome.java
+++ src/main/java/by/andd3dfx/string/Palindrome.java
@@ -1,10 +1,10 @@
 package by.andd3dfx.string;
 
-/*
-A palindrome is a word that reads the same backward or forward.
-Write a function that checks if a given word is a palindrome. Character case should be ignored.
-For example, isPalindrome("Deleveled") should return true as character case should be ignored, resulting in "deleveled",
-which is a palindrome since it reads the same backward and forward.
+/**
+ * A palindrome is a word that reads the same backward or forward. Write a function that checks if a given word is a
+ * palindrome. Character case should be ignored. For example, isPalindrome("Deleveled") should return true as character
+ * case should be ignored, resulting in "deleveled", which is a palindrome since it reads the same backward and
+ * forward.
  */
 public class Palindrome {
 
