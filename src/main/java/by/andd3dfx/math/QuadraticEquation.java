69eb9ed3fbc8c04c7ee16b2f58d3ed836b153039
Andrei Punko <andd3dfx@gmail.com>
2022-05-25 11:51:20 +0300

    Add QuadraticEquation solution

---
+++ src/main/java/by/andd3dfx/math/QuadraticEquation.java
@@ -0,0 +1,15 @@
+package by.andd3dfx.math;
+
+/**
+ * Реализовать функцию решения уравнения вида ax^2 + bx + c = 0.
+ */
+public class QuadraticEquation {
+
+    public static double[] solve(int a, int b, int c) {
+        var d = b * b - 4 * a * c;
+        if (d < 0) {
+            return null;
+        }
+        return new double[]{(-b - Math.sqrt(d)) / (2 * a), (-b + Math.sqrt(d)) / (2 * a)};
+    }
+}
---
+++ src/test/java/by/andd3dfx/math/QuadraticEquationTest.java
@@ -0,0 +1,15 @@
+package by.andd3dfx.math;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class QuadraticEquationTest {
+
+    @Test
+    public void solve() {
+        assertThat(QuadraticEquation.solve(1, -2, 1)).isEqualTo(new double[]{1.0, 1.0});
+        assertThat(QuadraticEquation.solve(1, -5, 6)).isEqualTo(new double[]{2.0, 3.0});
+        assertThat(QuadraticEquation.solve(1, -2, 5)).isEqualTo(null);
+    }
+}
