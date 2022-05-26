bcb58849ef17dc09904ce56b7f017e432048bde0
Andrei Punko <andd3dfx@gmail.com>
2022-05-26 09:54:58 +0300

    Optimize FindMaxEvenSum: avoid counting of odd numbers

--- src/main/java/by/andd3dfx/search/FindMaxEvenSum.java
+++ src/main/java/by/andd3dfx/search/FindMaxEvenSum.java
@@ -13,30 +13,31 @@ public class FindMaxEvenSum {
      * Т.к. все числа натуральные, т.е. положительные, а нам нужна максимальная сумма -
      * то из нечетных войдут все либо все кроме минимального.
      * <p>
-     * В итоге решением будет проход по всем числам с одновременным поиском минимального нечетного числа и
-     * подсчетом кол-ва нечетных чисел.
+     * В итоге решением будет одинарный проход по всем числам с поиском минимального нечетного числа и подсчетом суммы.
+     * Поток из суммы вычитаем это минимально нечетное, если сумма нечетная.
      */
     public static int find(int[] a) {
-        int oddCount = 0;
         int minOdd = -1;
         int sum = 0;
+
         for (int item : a) {
             sum += item;
             if (item % 2 == 1) {
-                oddCount++;
+
                 if (minOdd == -1) {
                     minOdd = item;
                     continue;
                 }
+
                 if (item < minOdd) {
                     minOdd = item;
                 }
             }
         }
 
-        if (oddCount % 2 == 0) {
-            return sum;
+        if (sum % 2 == 1) {
+            return sum - minOdd;
         }
-        return sum - minOdd;
+        return sum;
     }
 }
