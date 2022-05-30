25ce2f0bea759f5025ef3669b3205c76b284d302
Andrei Punko <andd3dfx@gmail.com>
2022-05-30 12:53:47 +0300

    Adjust RaiseToPower solution

--- src/main/java/by/andd3dfx/numeric/RaiseToPower.java
+++ src/main/java/by/andd3dfx/numeric/RaiseToPower.java
@@ -1,12 +1,12 @@
 package by.andd3dfx.numeric;
 
-import java.util.Map;
 import java.util.NavigableSet;
 import java.util.TreeMap;
 
-/*
-  Raise number to power
-  Based on "Stephens - Essential Algorithms"
+/**
+ * Raise number to power
+ * <p>
+ * Based on "Stephens - Essential Algorithms"
  */
 public class RaiseToPower {
 
@@ -16,14 +16,14 @@ public class RaiseToPower {
     }
 
     // Вычисляем a, a^2 , a^4 , a^8 и т.д., пока не получим значение a^N , где N + 1 > p
-    Map<Integer, Float> map = new TreeMap<>();
+    TreeMap<Integer, Float> map = new TreeMap<>();
     map.put(1, a);
     for (int power = 1; 2 * power <= p; power *= 2) {
       map.put(2 * power, map.get(power) * map.get(power));
     }
 
     // Используем эти степени a, чтобы вычислить a^P
-    NavigableSet<Integer> descendingKeySet = ((TreeMap<Integer, Float>) map).descendingKeySet();
+    NavigableSet<Integer> descendingKeySet = map.descendingKeySet();
     float result = 1;
     for (Integer power : descendingKeySet) {
       while (p >= power) {
@@ -35,4 +35,8 @@ public class RaiseToPower {
 
     return result;
   }
+
+    public static void main(String[] args) {
+        raise(2, 6);
+    }
 }
