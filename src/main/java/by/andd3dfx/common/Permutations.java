58b06c66d5360f0c38d05ead9179ded19171770a
Andrei Punko <andd3dfx@gmail.com>
2022-05-30 12:29:33 +0300

    Add solution for Permutations task

---
+++ src/main/java/by/andd3dfx/common/Permutations.java
@@ -0,0 +1,50 @@
+package by.andd3dfx.common;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * https://leetcode.com/problems/permutations/
+ * <p>
+ * Given an array nums of distinct integers, return all the possible permutations.
+ * You can return the answer in any order.
+ */
+public class Permutations {
+
+    public static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        populate(result, 0, nums);
+        return result;
+    }
+
+    private static void populate(List<List<Integer>> result, int start, int[] nums) {
+        if (start == nums.length) {
+            result.add(toList(nums));
+            return;
+        }
+
+        for (int k = start; k < nums.length; k++) {
+            swap(nums, start, k);
+            populate(result, start + 1, nums);
+            swap(nums, start, k);
+        }
+    }
+
+    private static List<Integer> toList(int[] nums) {
+        return Arrays.stream(nums).mapToObj(value -> value).collect(Collectors.toList());
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        var tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(
+                permute(new int[]{1, 2, 3})
+        );
+    }
+}
---
+++ src/test/java/by/andd3dfx/common/PermutationsTest.java
@@ -0,0 +1,79 @@
+package by.andd3dfx.common;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PermutationsTest {
+
+    @Test
+    public void permute1() {
+        List<List<Integer>> result = Permutations.permute(new int[]{1});
+        assertThat(result)
+                .hasSameElementsAs(List.of(
+                                List.of(1)
+                        )
+                );
+    }
+
+    @Test
+    public void permute2() {
+        assertThat(Permutations.permute(new int[]{1, 2}))
+                .hasSameElementsAs(List.of(
+                                List.of(1, 2),
+                                List.of(2, 1)
+                        )
+                );
+    }
+
+    @Test
+    public void permute3() {
+        assertThat(Permutations.permute(new int[]{1, 2, 3}))
+                .hasSameElementsAs(List.of(
+                                List.of(1, 2, 3),
+                                List.of(1, 3, 2),
+                                List.of(2, 1, 3),
+                                List.of(2, 3, 1),
+                                List.of(3, 1, 2),
+                                List.of(3, 2, 1)
+                        )
+                );
+    }
+
+    @Test
+    public void permute4() {
+        assertThat(Permutations.permute(new int[]{1, 2, 3, 4}))
+                .hasSameElementsAs(List.of(
+                                List.of(1, 2, 3, 4),
+                                List.of(1, 2, 4, 3),
+                                List.of(1, 3, 2, 4),
+                                List.of(1, 3, 4, 2),
+                                List.of(1, 4, 2, 3),
+                                List.of(1, 4, 3, 2),
+
+                                List.of(2, 1, 3, 4),
+                                List.of(2, 1, 4, 3),
+                                List.of(2, 3, 1, 4),
+                                List.of(2, 3, 4, 1),
+                                List.of(2, 4, 1, 3),
+                                List.of(2, 4, 3, 1),
+
+                                List.of(3, 1, 2, 4),
+                                List.of(3, 1, 4, 2),
+                                List.of(3, 2, 1, 4),
+                                List.of(3, 2, 4, 1),
+                                List.of(3, 4, 1, 2),
+                                List.of(3, 4, 2, 1),
+
+                                List.of(4, 1, 2, 3),
+                                List.of(4, 1, 3, 2),
+                                List.of(4, 2, 1, 3),
+                                List.of(4, 2, 3, 1),
+                                List.of(4, 3, 1, 2),
+                                List.of(4, 3, 2, 1)
+                        )
+                );
+    }
+}
