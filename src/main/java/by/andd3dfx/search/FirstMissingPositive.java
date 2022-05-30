e733334f99a4b15aec0f7977eaad967deeaf470c
Andrei Punko <andd3dfx@gmail.com>
2022-05-30 12:28:52 +0300

    Add solution for FirstMissingPositive task

---
+++ src/main/java/by/andd3dfx/search/FirstMissingPositive.java
@@ -0,0 +1,32 @@
+package by.andd3dfx.search;
+
+/**
+ * https://leetcode.com/problems/first-missing-positive/
+ * <p>
+ * Given an unsorted integer array nums, return the smallest missing positive integer.
+ * <p>
+ * You must implement an algorithm that runs in O(n) time and uses constant extra space.
+ */
+public class FirstMissingPositive {
+
+    /**
+     * Good idea was that if we have n-length array, the smallest missing positive integer will not be greater than n
+     */
+    public static int find(int[] nums) {
+        int[] presence = new int[nums.length];
+
+        for (int num : nums) {
+            if (num > 0 && num < nums.length) {
+                presence[num] = 1;
+            }
+        }
+
+        int position = 1;
+        for (; position < presence.length; position++) {
+            if (presence[position] == 0) {
+                return position;
+            }
+        }
+        return presence.length + 1;
+    }
+}
