b24cee78c068e08fae57b58ee028a36dae427f66
Andrei Punko <andd3dfx@gmail.com>
2022-06-06 13:47:36 +0300

    Address TODO: parameterize BucketSort

--- src/main/java/by/andd3dfx/sorting/BucketSort.java
+++ src/main/java/by/andd3dfx/sorting/BucketSort.java
@@ -3,43 +3,64 @@ package by.andd3dfx.sorting;
 import java.util.ArrayList;
 import java.util.List;
 
-// TODO: parameterize this sorter
-public class BucketSort extends AbstractSort<Long> {
+public class BucketSort extends AbstractSort {
 
-  private final int BUCKETS_COUNT = 10;
+    private final int BUCKETS_COUNT = 10;
 
-  @Override
-  public void sort() {
-    // Prepare empty buckets
-    List<List<Long>> buckets = new ArrayList<>(BUCKETS_COUNT);
-    for (int i = 0; i < BUCKETS_COUNT; i++) {
-      buckets.add(new ArrayList<>());
+    @Override
+    public void sort() {
+        // Prepare empty buckets
+        List<List<Comparable>> buckets = new ArrayList<>(BUCKETS_COUNT);
+        for (int i = 0; i < BUCKETS_COUNT; i++) {
+            buckets.add(new ArrayList<>());
+        }
+
+        // Fill in buckets
+        for (int i = 0; i < items.length; i++) {
+            buckets.get(determineBucketIndex(items[i], BUCKETS_COUNT)).add(items[i]);
+        }
+
+        int currIndex = 0;
+        for (int bucketIndex = 0; bucketIndex < BUCKETS_COUNT; bucketIndex++) {
+            List<Comparable> bucket = buckets.get(bucketIndex);
+
+            // Sort elements in each bucket
+            InsertionSort sorter = new InsertionSort();
+            sorter.init(bucket.toArray(new Comparable[0]));
+            sorter.sort();
+
+            // Populate result array with values from bucket
+            for (var item : sorter.items) {
+                items[currIndex] = item;
+                currIndex++;
+            }
+        }
     }
 
-    // Fill in buckets
-    for (int i = 0; i < items.length; i++) {
-      buckets.get(determineBucketIndex(items[i], BUCKETS_COUNT)).add(items[i]);
+    /**
+     * Not found better way, but sorter parameterized now
+     */
+    private int determineBucketIndex(Comparable element, int bucketsCount) {
+        if (element instanceof Integer) {
+            return (int) ((bucketsCount - 1) *
+                    ((Integer) element - (double) Integer.MIN_VALUE) / ((double) Integer.MAX_VALUE - (double) Integer.MIN_VALUE));
+        } else if (element instanceof Long) {
+            return (int) ((bucketsCount - 1) *
+                    ((Long) element - (double) Long.MIN_VALUE) / ((double) Long.MAX_VALUE - (double) Long.MIN_VALUE));
+        } else if (element instanceof Double) {
+            return (int) ((bucketsCount - 1) *
+                    ((Double) element - Double.MIN_VALUE) / (Double.MAX_VALUE - Double.MIN_VALUE));
+        } else if (element instanceof Float) {
+            return (int) ((bucketsCount - 1) *
+                    ((Float) element - Float.MIN_VALUE) / (Float.MAX_VALUE - Float.MIN_VALUE));
+        } else if (element instanceof Byte) {
+            return (bucketsCount - 1) *
+                    ((Byte) element - Byte.MIN_VALUE) / (Byte.MAX_VALUE - Byte.MIN_VALUE);
+        } else if (element instanceof Short) {
+            return (bucketsCount - 1) *
+                    ((Short) element - Byte.MIN_VALUE) / (Short.MAX_VALUE - Short.MIN_VALUE);
+        }
+
+        throw new UnsupportedOperationException("This type is not supported!");
     }
-
-    int currIndex = 0;
-    for (int bucketIndex = 0; bucketIndex < BUCKETS_COUNT; bucketIndex++) {
-      List<Long> bucket = buckets.get(bucketIndex);
-
-      // Sort elements in each bucket
-      InsertionSort sorter = new InsertionSort();
-      sorter.init(bucket.toArray(new Long[0]));
-      sorter.sort();
-
-      // Populate result array with values from bucket
-      for (var item : (Long[])sorter.items) {
-        items[currIndex] = item;
-        currIndex++;
-      }
-    }
-  }
-
-  private int determineBucketIndex(long element, int bucketsCount) {
-    return (int) ((bucketsCount - 1) *
-        ((double) element - (double) Long.MIN_VALUE) / ((double) Long.MAX_VALUE - (double) Long.MIN_VALUE));
-  }
 }
