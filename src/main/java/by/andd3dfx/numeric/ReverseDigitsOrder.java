0e7f5fdacd97aed9fd629abb65aced967610f1a1
Andrei Punko <andd3dfx@gmail.com>
2022-11-21 11:05:37 +0300

    Adjustment for ReverseDigitsOrder task

--- src/main/java/by/andd3dfx/numeric/ReverseDigitsOrder.java
+++ src/main/java/by/andd3dfx/numeric/ReverseDigitsOrder.java
@@ -1,18 +1,25 @@
 package by.andd3dfx.numeric;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
- * Revert order of digits in Integer number
+ * Revert order of digits in Integer number: 12389->98321
  */
 public class ReverseDigitsOrder {
 
-  public static int perform(int number) {
-    int result = 0;
-    do {
-      int digit = number % 10;
-      result = result * 10 + digit;
-      number /= 10;
-    } while (number > 0);
+    public static int usingString(int number) {
+        String string = String.valueOf(number);
+        String reversedString = StringUtils.reverse(string);
+        return Integer.valueOf(reversedString);
+    }
 
-    return result;
-  }
+    public static int usingNumber(int number) {
+        int result = 0;
+        do {
+            int digit = number % 10;
+            result = result * 10 + digit;
+            number /= 10;
+        } while (number > 0);
+        return result;
+    }
 }
--- src/test/java/by/andd3dfx/numeric/ReverseDigitsOrderTest.java
+++ src/test/java/by/andd3dfx/numeric/ReverseDigitsOrderTest.java
@@ -1,14 +1,23 @@
 package by.andd3dfx.numeric;
 
+import org.junit.Test;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-import org.junit.Test;
-
 public class ReverseDigitsOrderTest {
 
-  @Test
-  public void reverse() {
-    assertThat("Wrong result", ReverseDigitsOrder.perform(12369), is(96321));
-  }
+    @Test
+    public void usingString() {
+        assertThat(ReverseDigitsOrder.usingString(1), is(1));
+        assertThat(ReverseDigitsOrder.usingString(23), is(32));
+        assertThat(ReverseDigitsOrder.usingString(123904), is(409321));
+    }
+
+    @Test
+    public void usingNumber() {
+        assertThat(ReverseDigitsOrder.usingNumber(1), is(1));
+        assertThat(ReverseDigitsOrder.usingNumber(23), is(32));
+        assertThat(ReverseDigitsOrder.usingNumber(123904), is(409321));
+    }
 }
