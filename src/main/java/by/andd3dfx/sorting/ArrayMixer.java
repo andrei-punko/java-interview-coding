ace4338fe83e26ad50a0fcd3923a17c9cbce83ab
Andrei Punko <andd3dfx@gmail.com>
2022-11-23 10:11:37 +0300

    Add ArrayMixer

---
+++ src/main/java/by/andd3dfx/sorting/ArrayMixer.java
@@ -0,0 +1,25 @@
+package by.andd3dfx.sorting;
+
+import java.util.Random;
+
+/**
+ * Mix array
+ */
+public class ArrayMixer {
+
+    private static final Random random = new Random();
+
+    public static <T> void apply(T[] array) {
+        for (int iteration = 0; iteration <= array.length / 2; iteration++) {
+            var i = random.nextInt(array.length);
+            var j = random.nextInt(array.length);
+            swap(array, i, j);
+        }
+    }
+
+    private static <T> void swap(T[] array, int i, int j) {
+        var tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+    }
+}
---
+++ src/test/java/by/andd3dfx/sorting/ArrayMixerTest.java
@@ -0,0 +1,37 @@
+package by.andd3dfx.sorting;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ArrayMixerTest {
+
+    private final Integer[] INTEGERS = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    private final String[] STRINGS = {"Aasd", "asda", "rteg", "f", "677g"};
+
+    @Test
+    public void applyForInteger() {
+        var array = INTEGERS.clone();
+        assertThat(array).isEqualTo(INTEGERS);
+        System.out.println(Arrays.toString(array));
+
+        ArrayMixer.apply(array);
+
+        assertThat(array).isNotEqualTo(INTEGERS);
+        System.out.println(Arrays.toString(array));
+    }
+
+    @Test
+    public void applyForString() {
+        var array = STRINGS.clone();
+        assertThat(array).isEqualTo(STRINGS);
+        System.out.println(Arrays.toString(array));
+
+        ArrayMixer.apply(array);
+
+        assertThat(array).isNotEqualTo(STRINGS);
+        System.out.println(Arrays.toString(array));
+    }
+}
