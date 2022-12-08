f66d38844a145f8535ace72b34dc5ca63307eb84
Andrei Punko <andd3dfx@gmail.com>
2022-12-08 11:13:34 +0300

    Adjust GreatestCommonDivisor task

--- src/main/java/by/andd3dfx/numeric/GreatestCommonDivisor.java
+++ src/main/java/by/andd3dfx/numeric/GreatestCommonDivisor.java
@@ -1,17 +1,28 @@
 package by.andd3dfx.numeric;
 
 /**
- * Determine the greatest common divisor
- * Based on "Stephens - Essential Algorithms"
+ * <pre>
+ * Determine the greatest common divisor.
+ * Based on "Stephens - Essential Algorithms":
+ *   GCD(a, b) = GCD(b, a Mod b)
+ *   GCD(a, 0) is a.
+ * </pre>
  */
 public class GreatestCommonDivisor {
 
-  public static int determineGCD(int a, int b) {
-    while (b != 0) {
-      int remainder = a % b;
-      a = b;
-      b = remainder;
+    public static int determineUsingRecursion(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return determineUsingRecursion(b, a % b);
     }
-    return a;
-  }
+
+    public static int determineUsingLoop(int a, int b) {
+        while (b != 0) {
+            int remainder = a % b;
+            a = b;
+            b = remainder;
+        }
+        return a;
+    }
 }
--- src/test/java/by/andd3dfx/numeric/GreatestCommonDivisorTest.java
+++ src/test/java/by/andd3dfx/numeric/GreatestCommonDivisorTest.java
@@ -1,16 +1,23 @@
 package by.andd3dfx.numeric;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 public class GreatestCommonDivisorTest {
 
-  @Test
-  public void determineGCD() {
-    assertThat("GCD(30, 21) = 3", GreatestCommonDivisor.determineGCD(30, 21), is(3));
-    assertThat("GCD(31, 17) = 1", GreatestCommonDivisor.determineGCD(31, 17), is(1));
-    assertThat("GCD(64, 48) = 16", GreatestCommonDivisor.determineGCD(64, 48), is(16));
-  }
+    @Test
+    public void determineUsingRecursion() {
+        assertThat(GreatestCommonDivisor.determineUsingRecursion(30, 21), is(3));
+        assertThat(GreatestCommonDivisor.determineUsingRecursion(31, 17), is(1));
+        assertThat(GreatestCommonDivisor.determineUsingRecursion(64, 48), is(16));
+    }
+
+    @Test
+    public void determineUsingLoop() {
+        assertThat(GreatestCommonDivisor.determineUsingLoop(30, 21), is(3));
+        assertThat(GreatestCommonDivisor.determineUsingLoop(31, 17), is(1));
+        assertThat(GreatestCommonDivisor.determineUsingLoop(64, 48), is(16));
+    }
 }
