a7309c61ad12a57cc28b5d39c4237209afe06a71
Andrei Punko <andd3dfx@gmail.com>
2022-12-20 11:16:40 +0300

    Adjust BinarySearchTree task

--- src/main/java/by/andd3dfx/tree/BinarySearchTree.java
+++ src/main/java/by/andd3dfx/tree/BinarySearchTree.java
@@ -1,46 +1,43 @@
 package by.andd3dfx.tree;
 
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+
 /**
+ * <pre>
  * Binary search tree (BST) is a binary tree where the value of each node is larger or equal to the values in all
  * the nodes in that node's left subtree and is smaller than the values in all the nodes in that node's right subtree.
  * Write a function that checks if a given binary search tree contains a given value.
  *
  * For example, for the following tree:
- *     n1 (Value: 1, Left: null, Right: null)
- *     n2 (Value: 2, Left: n1, Right: n3)
- *     n3 (Value: 3, Left: null, Right: null)
- * Call to contains(n2, 3) should return true since a tree with root at n2 contains number 3.
+ * n1 (Value: 1, Left: null, Right: null)
+ * n2 (Value: 2, Left: n1, Right: n3)
+ * n3 (Value: 3, Left: null, Right: null)
+ * Call contains(n2, 3) should return true since a tree with root at n2 contains number 3.
+ * </pre>
  */
-class Node {
-
-  public int value;
-  public Node left, right;
-
-  public Node(int value, Node left, Node right) {
-    this.value = value;
-    this.left = left;
-    this.right = right;
-  }
-}
-
 public class BinarySearchTree {
 
-  public static boolean contains(Node root, int value) {
-    if (root == null) {
-      return false;
-    }
-    if (root.value == value) {
-      return true;
-    }
-    Node node = (value < root.value) ? root.left : root.right;
-    return contains(node, value);
-  }
+    @Builder
+    @RequiredArgsConstructor
+    public static class Node {
 
-  public static void main(String[] args) {
-    Node n1 = new Node(1, null, null);
-    Node n3 = new Node(3, null, null);
-    Node n2 = new Node(2, n1, n3);
+        private final int value;
+        private final Node left, right;
 
-    System.out.println(contains(n2, 3));
-  }
+        public boolean contains(int value) {
+            return contains(this, value);
+        }
+
+        public static boolean contains(Node node, int value) {
+            if (node == null) {
+                return false;
+            }
+            if (node.value == value) {
+                return true;
+            }
+            var nextNode = (value < node.value) ? node.left : node.right;
+            return contains(nextNode, value);
+        }
+    }
 }
--- src/test/java/by/andd3dfx/tree/BinarySearchTreeTest.java
+++ src/test/java/by/andd3dfx/tree/BinarySearchTreeTest.java
@@ -1,19 +1,60 @@
 package by.andd3dfx.tree;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-
+import by.andd3dfx.tree.BinarySearchTree.Node;
 import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class BinarySearchTreeTest {
 
-  @Test
-  public void contains() {
-    Node n1 = new Node(1, null, null);
-    Node n3 = new Node(3, null, null);
-    Node n2 = new Node(2, n1, n3);
+    /**
+     * <pre>
+     *      5
+     *     / \
+     *    2   7
+     *   /   / \
+     *  1   6   8
+     * </pre>
+     */
+    @Test
+    public void testContains() {
+        var n1 = new Node(1, null, null);
+        var n6 = new Node(6, null, null);
+        var n8 = new Node(8, null, null);
+        var n2 = new Node(2, n1, null);
+        var n7 = new Node(7, n6, n8);
+        var n5 = new Node(5, n2, n7);
 
-    assertThat("Contains 3", BinarySearchTree.contains(n2, 3), is(true));
-    assertThat("Not contains 3", BinarySearchTree.contains(n1, 3), is(false));
-  }
+        assertTrue(Node.contains(n5, 1));
+        assertTrue(Node.contains(n5, 2));
+        assertTrue(Node.contains(n5, 5));
+        assertTrue(Node.contains(n5, 6));
+        assertTrue(Node.contains(n5, 7));
+        assertTrue(Node.contains(n5, 8));
+        assertFalse(Node.contains(n5, 4));
+        assertTrue(n5.contains(1));
+        assertTrue(n5.contains(2));
+        assertTrue(n5.contains(5));
+        assertTrue(n5.contains(6));
+        assertTrue(n5.contains(7));
+        assertTrue(n5.contains(8));
+        assertFalse(n5.contains(4));
+
+        assertTrue(Node.contains(n2, 1));
+        assertTrue(Node.contains(n2, 2));
+        assertFalse(Node.contains(n2, 7));
+        assertTrue(n2.contains(1));
+        assertTrue(n2.contains(2));
+        assertFalse(n2.contains(7));
+
+        assertTrue(Node.contains(n7, 6));
+        assertTrue(Node.contains(n7, 7));
+        assertTrue(Node.contains(n7, 8));
+        assertFalse(Node.contains(n7, 2));
+        assertTrue(n7.contains(6));
+        assertTrue(n7.contains(7));
+        assertTrue(n7.contains(8));
+        assertFalse(n7.contains(2));
+    }
 }
