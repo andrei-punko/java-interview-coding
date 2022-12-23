1447fd6de7af9586c09e937e76a8cb9008dca599
Andrei Punko <andd3dfx@gmail.com>
2022-12-23 17:01:21 +0300

    Adjust MakeStringCompact task. Remove duplicate task

--- src/main/java/by/andd3dfx/string/MakeStringCompact.java
+++ src/main/java/by/andd3dfx/string/MakeStringCompact.java
@@ -2,33 +2,33 @@ package by.andd3dfx.string;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
- * Convert incoming string into string with char frequencies: AAABBCDDDDEFFFFFFF -> A3B2CD4EF7
+ * <pre>
+ * Write a function that transforms string into a new string. New string does not contain repeating letters
+ * but contains a number after every letter that means how many times the letter was repeated in the original string.
+ * a.	“” -> “”
+ * b.	“a” -> “a1”
+ * c.	“aaa” -> “a3”
+ * d.	“aaabbc” -> “a3b2c1”
+ * </pre>
  */
 public class MakeStringCompact {
 
-    public static String convert(String str) {
+    public static String build(String s) {
         Map<Character, Integer> map = new LinkedHashMap<>();
-
-        str.chars().forEach(item -> {
-            char character = (char) item;
-
-            int value = 1;
-            if (map.containsKey(character)) {
-                value += map.get(character);
+        for (char ch : s.toCharArray()) {
+            if (map.containsKey(ch)) {
+                map.put(ch, map.get(ch) + 1);
+            } else {
+                map.put(ch, 1);
             }
-            map.put(character, value);
-        });
+        }
 
-        return map.entrySet().stream()
-            .map(entry -> {
-                if (entry.getValue() == 1) {
-                    return entry.getKey().toString();
-                }
-                return String.format("%s%d", entry.getKey(), entry.getValue());
-            })
-            .collect(Collectors.joining());
+        StringBuilder stringBuilder = new StringBuilder();
+        for (var key : map.keySet()) {
+            stringBuilder.append(String.format("%s%d", key, map.get(key)));
+        }
+        return stringBuilder.toString();
     }
 }
--- src/test/java/by/andd3dfx/string/MakeStringCompactTest.java
+++ src/test/java/by/andd3dfx/string/MakeStringCompactTest.java
@@ -1,16 +1,20 @@
 package by.andd3dfx.string;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.Test;
+
+import static by.andd3dfx.string.MakeStringCompact.build;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class MakeStringCompactTest {
 
     @Test
-    public void convert() {
-        assertThat(MakeStringCompact.convert("")).isEqualTo("");
-        assertThat(MakeStringCompact.convert("A")).isEqualTo("A");
-        assertThat(MakeStringCompact.convert("ABC")).isEqualTo("ABC");
-        assertThat(MakeStringCompact.convert("AAABBCDDDDEFFFFFFFFFF")).isEqualTo("A3B2CD4EF10");
+    public void testBuild() {
+        assertThat(build(""), is(""));
+        assertThat(build("a"), is("a1"));
+        assertThat(build("aaa"), is("a3"));
+        assertThat(build("aaabbc"), is("a3b2c1"));
+        assertThat(build("aaacbb"), is("a3c1b2"));
+        assertThat(build("aaacbbccc"), is("a3c4b2"));
     }
 }
