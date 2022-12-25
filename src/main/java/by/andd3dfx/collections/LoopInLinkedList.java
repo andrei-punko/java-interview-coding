6b75d10e1b7f791a0ad91f9c3067e2587e076e95
Andrei Punko <andd3dfx@gmail.com>
2022-12-25 18:09:48 +0300

    Add LoopInLinkedList task solution

---
+++ src/main/java/by/andd3dfx/collections/LoopInLinkedList.java
@@ -0,0 +1,45 @@
+package by.andd3dfx.collections;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * Find loop in the linked list
+ */
+public class LoopInLinkedList {
+
+    @Data
+    @AllArgsConstructor
+    public static class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public static boolean isPresent(Node head) {
+        var slow = head;
+        var fast = head;
+
+        while (true) {
+            fast = fast.next;
+            if (fast == slow) {
+                return true;
+            }
+            if (fast == null) {
+                return false;
+            }
+
+            slow = slow.next;
+            fast = fast.next;
+            if (fast == slow) {
+                return true;
+            }
+            if (fast == null) {
+                return false;
+            }
+        }
+    }
+}
---
+++ src/test/java/by/andd3dfx/collections/LoopInLinkedListTest.java
@@ -0,0 +1,48 @@
+package by.andd3dfx.collections;
+
+import by.andd3dfx.collections.LoopInLinkedList.Node;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class LoopInLinkedListTest {
+
+    @Test
+    public void findForSingleNode() {
+        Node head = new Node(10);
+
+        assertFalse(LoopInLinkedList.isPresent(head));
+    }
+
+    @Test
+    public void find() {
+        Node head = new Node(10, new Node(12, new Node(13)));
+
+        assertFalse(LoopInLinkedList.isPresent(head));
+    }
+
+    @Test
+    public void findForListWithLoop() {
+        Node node1 = new Node(10);
+        Node node2 = new Node(11, node1);
+        Node node3 = new Node(12, node2);
+        Node node4 = new Node(13, node3);
+        Node node5 = new Node(14, node4);
+        node1.setNext(node3);
+
+        assertTrue(LoopInLinkedList.isPresent(node5));
+        assertTrue(LoopInLinkedList.isPresent(node4));
+        assertTrue(LoopInLinkedList.isPresent(node3));
+        assertTrue(LoopInLinkedList.isPresent(node2));
+        assertTrue(LoopInLinkedList.isPresent(node1));
+    }
+
+    @Test
+    public void findForSingleNodeWithLoop() {
+        Node head = new Node(10);
+        head.setNext(head);
+
+        assertTrue(LoopInLinkedList.isPresent(head));
+    }
+}
