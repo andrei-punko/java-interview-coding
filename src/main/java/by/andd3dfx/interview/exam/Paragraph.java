7e8864633143e9cd0811a345843bf0b269f4325f
Andrei Punko <andd3dfx@gmail.com>
2023-01-06 15:33:19 +0300

    Adjust Paragraph task

--- src/main/java/by/andd3dfx/interview/exam/Paragraph.java
+++ src/main/java/by/andd3dfx/interview/exam/Paragraph.java
@@ -8,31 +8,29 @@ import java.util.regex.Pattern;
  * (where X, Y and Z each represent the digits 0-9). Write a method that re-format all policy numbers in a
  * well-formatted paragraph ('-' may appear elsewhere in the text).
  * <p>
- * For example, changeFormat("Please quote your number: 112-39-8552.") should return
- * "Please quote your policy number: 112/8552/39".
+ * For example, changeFormat("Please quote your policy number: 112-39-8552.") should return
+ * "Please quote your policy number: 112/8552/39.".
  */
 public class Paragraph {
 
-  private static final String PATTERN_1 = "\\d\n*\\d\n*\\d\n*-\n*\\d\n*\\d\n*-\n*\\d\n*\\d\n*\\d\n*\\d";
-  private static final String PATTERN_2 = "(\\d{3}[^-]*)-([^-]*\\d{2}[^-]*)-([^-]*\\d{4})";
+  private static final Pattern PATTERN_1 = Pattern.compile("\\d{3}\n*-\n*\\d{2}\n*-\n*\\d{4}");
+  private static final Pattern PATTERN_2 = Pattern.compile("(\\d{3}\n*)-(\n*\\d{2}\n*)-(\n*\\d{4})");
 
   public static String changeFormat(String paragraph) {
-    Pattern p = Pattern.compile(PATTERN_1);
-    Matcher m = p.matcher(paragraph);
-    while (m.find()) {
-      String s = m.group(0);
+    Matcher matcher = PATTERN_1.matcher(paragraph);
+    while (matcher.find()) {
+      String s = matcher.group(0);
       String[] parts = s.split("-");
       String result = parts[0] + "/" + parts[2] + "/" + parts[1];
-      paragraph = paragraph.replaceAll(m.group(0), result);
+      paragraph = paragraph.replaceAll(matcher.group(0), result);
     }
     return paragraph;
   }
 
-  public static String changeFormat2(String paragraph) {
-    Pattern p = Pattern.compile(PATTERN_2);
-    Matcher m = p.matcher(paragraph);
-    while (m.find()) {
-      paragraph = m.replaceAll("$1/$3/$2");
+  public static String changeFormatUsingRegExGroups(String paragraph) {
+    Matcher matcher = PATTERN_2.matcher(paragraph);
+    while (matcher.find()) {
+      paragraph = matcher.replaceAll("$1/$3/$2");
     }
     return paragraph;
   }
--- src/test/java/by/andd3dfx/interview/exam/ParagraphTest.java
+++ src/test/java/by/andd3dfx/interview/exam/ParagraphTest.java
@@ -1,7 +1,7 @@
 package by.andd3dfx.interview.exam;
 
 import static by.andd3dfx.interview.exam.Paragraph.changeFormat;
-import static by.andd3dfx.interview.exam.Paragraph.changeFormat2;
+import static by.andd3dfx.interview.exam.Paragraph.changeFormatUsingRegExGroups;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -34,26 +34,26 @@ public class ParagraphTest {
   }
 
   @Test
-  public void changeFormat2NothingChanged() {
-    assertThat(changeFormat2("Please quote your policy number."),
+  public void changeFormatUsingRegExGroupsNothingChanged() {
+    assertThat(changeFormatUsingRegExGroups("Please quote your policy number."),
         is("Please quote your policy number."));
   }
 
   @Test
-  public void changeFormat2Simple() {
-    assertThat(changeFormat2("Please quote your policy number: 112-39-8552."),
+  public void changeFormatUsingRegExGroupsSimple() {
+    assertThat(changeFormatUsingRegExGroups("Please quote your policy number: 112-39-8552."),
         is("Please quote your policy number: 112/8552/39."));
   }
 
   @Test
-  public void changeFormat2MultipleParagraphs() {
-    assertThat(changeFormat2("Please quote your policy numbers: 112-39-8552 and 134-39-9552."),
+  public void changeFormatUsingRegExGroupsMultipleParagraphs() {
+    assertThat(changeFormatUsingRegExGroups("Please quote your policy numbers: 112-39-8552 and 134-39-9552."),
         is("Please quote your policy numbers: 112/8552/39 and 134/9552/39."));
   }
 
   @Test
-  public void changeFormat2WithNewStringDelimiter() {
-    assertThat(changeFormat2("Please quote your policy number: 112-39-\n8552."),
+  public void changeFormatUsingRegExGroupsWithNewStringDelimiter() {
+    assertThat(changeFormatUsingRegExGroups("Please quote your policy number: 112-39-\n8552."),
         is("Please quote your policy number: 112/\n8552/39."));
   }
 }
