22f5615c48395bc76f3cf2fe3e2b3669a4c6a8fd
Andrei Punko <andd3dfx@gmail.com>
2023-01-15 15:22:35 +0300

    Merge all Palindrom tasks into one class

--- src/main/java/by/andd3dfx/string/Palindrome.java
+++ src/main/java/by/andd3dfx/string/Palindrome.java
@@ -1,18 +1,32 @@
 package by.andd3dfx.string;
 
-/**
- * A palindrome is a word that reads the same backward or forward. Write a function that checks if a given word is a
- * palindrome. Character case should be ignored. For example, isPalindrome("Deleveled") should return true as character
- * case should be ignored, resulting in "deleveled", which is a palindrome since it reads the same backward and
- * forward.
- */
 public class Palindrome {
 
+  /**
+   * A palindrome is a word that reads the same backward or forward.
+   * Write a function that checks if a given word is a palindrome.
+   */
   public static boolean isPalindrome(String word) {
+    return isPalindrome(word, false);
+  }
+
+  /**
+   * A palindrome is a word that reads the same backward or forward. Write a function that checks if a given word is a
+   * palindrome. Character case should be ignored. For example, isPalindrome("Deleveled") should return true as
+   * character case should be ignored, resulting in "deleveled", which is a palindrome since it reads the same
+   * backward and forward.
+   */
+  public static boolean isPalindromeIgnoreCase(String word) {
+    return isPalindrome(word, true);
+  }
+
+  private static boolean isPalindrome(String word, boolean ignoreCase) {
     if (word == null) {
       return false;
     }
-    word = word.toLowerCase();
+    if (ignoreCase) {
+        word = word.toLowerCase();
+    }
 
     for (int i = 0; i < word.length() / 2; i++) {
       if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
@@ -22,27 +36,63 @@ public class Palindrome {
     return true;
   }
 
-  public static String removePalindromesFromString(String string) {
-    String[] words = string.split("\\s");
+  /**
+   * Remove palindromes from string.
+   */
+  public static String removePalindromes(String string) {
+    String[] words = string.split(",?\\s");
     for (String word : words) {
-      if (isPalindrome(word)) {
+      if (isPalindromeIgnoreCase(word)) {
           string = string.replace(word, "");
       }
     }
-    return string;
+    return string
+            .replaceAll("(,\\s){2,}", ", ")
+            .replaceAll("\\s{2,}", " ")
+            .trim();
   }
 
+  /**
+   * Remove Nth palindrome from string.
+   */
   public static String removeNthPalindrome(String string, int n) {
-      String[] words = string.split("\\s");
+      String[] words = string.split(",?\\s");
       int counter = 0;
       for (String word : words) {
-          if (isPalindrome(word)) {
+          if (isPalindromeIgnoreCase(word)) {
               counter++;
               if (counter == n) {
-                  return string.replace(word, "");
+                  return string
+                          .replace(word, "")
+                          .replaceAll("(,\\s){2,}", ", ")
+                          .replaceAll("\\s{2,}", " ")
+                          .trim();
               }
           }
       }
       return string;
+  }
+
+  /**
+   * <pre>
+   * Given a string, find the longest substring which is palindrome.
+   * For example:
+   * Input: "forgeeksskeegfor",	Output: "geeksskeeg"
+   * Input: "Geeks",			Output: "ee""
+   * </pre>
+   */
+  public static String longestPalindromeSubstring(String str) {
+      var result = str.substring(0, 1);
+
+      for (int i = 0; i < str.length(); i++) {
+          for (int j = str.length() - 1; j >= i + result.length() + 1; j--) {
+              var currentString = str.substring(i, j);
+              if (currentString.length() > result.length() && isPalindrome(currentString)) {
+                  result = currentString;
+              }
+          }
+      }
+
+      return result;
   }
 }
--- src/test/java/by/andd3dfx/string/PalindromeTest.java
+++ src/test/java/by/andd3dfx/string/PalindromeTest.java
@@ -1,29 +1,57 @@
 package by.andd3dfx.string;
 
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import static by.andd3dfx.string.Palindrome.isPalindrome;
+import static by.andd3dfx.string.Palindrome.isPalindromeIgnoreCase;
+import static by.andd3dfx.string.Palindrome.longestPalindromeSubstring;
+import static by.andd3dfx.string.Palindrome.removeNthPalindrome;
+import static by.andd3dfx.string.Palindrome.removePalindromes;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-
-import org.junit.Test;
 
 public class PalindromeTest {
 
   @Test
-  public void isPalindrome() {
-    assertThat("ertglgtre is palindrom", Palindrome.isPalindrome("ertglgtre"), is(true));
-    assertThat("Anna is palindrom (case insensitive)", Palindrome.isPalindrome("Anna"), is(true));
-    assertThat("Dumb is not palindrom", Palindrome.isPalindrome("Dumb"), is(false));
+  public void testIsPalindrome() {
+    assertThat(isPalindrome("ertglgtre"), is(true));
+    assertThat(isPalindrome("Anna"), is(false));
+    assertThat(isPalindrome("anna"), is(true));
+    assertThat(isPalindrome("Dumb"), is(false));
   }
 
   @Test
-  public void removePalindromesFromString() {
-    assertThat(Palindrome.removePalindromesFromString("assa Rome, Anna all liquid small mom"), is(" Rome,  all liquid small "));
+  public void testIsPalindromeIgnoreCase() {
+    assertThat(isPalindromeIgnoreCase("ertglgtre"), is(true));
+    assertThat(isPalindromeIgnoreCase("Anna"), is(true));
+    assertThat(isPalindromeIgnoreCase("Dumb"), is(false));
   }
 
   @Test
-  public void removeNthPalindrome() {
+  public void testRemovePalindromes() {
+    assertThat(removePalindromes("assa Rome Anna all liquid small mom"), is("Rome all liquid small"));
+    assertThat(removePalindromes("assa Rome, Anna, all liquid small mom"), is("Rome, all liquid small"));
+  }
+
+  @Test
+  public void testRemoveNthPalindrome() {
     assertThat("Second palindrome should be removed",
-        Palindrome.removeNthPalindrome("All assa Rome Anna small mom", 2), is("All assa Rome  small mom"));
+        removeNthPalindrome("All assa Rome Anna small mom", 2), is("All assa Rome small mom"));
     assertThat("String should should not be changed",
-        Palindrome.removeNthPalindrome("All assa Rome Anna small mom", 5), is("All assa Rome Anna small mom"));
+        removeNthPalindrome("All assa Rome Anna small mom", 5), is("All assa Rome Anna small mom"));
+
+    assertThat("Second palindrome should be removed",
+            removeNthPalindrome("All assa Rome, Anna, small mom", 2), is("All assa Rome, small mom"));
+    assertThat("String should should not be changed",
+            removeNthPalindrome("All assa Rome, Anna, small mom", 5), is("All assa Rome, Anna, small mom"));
+  }
+
+  @Test
+  public void testLongestPalindromeSubstring() {
+    Assertions.assertThat(longestPalindromeSubstring("forgeeksskeegfor")).isEqualTo("geeksskeeg");
+    Assertions.assertThat(longestPalindromeSubstring("Geeks")).isEqualTo("ee");
+    Assertions.assertThat(longestPalindromeSubstring("Composition")).isEqualTo("iti");
+    Assertions.assertThat(longestPalindromeSubstring("abbaerrtrre1ll1")).isEqualTo("errtrre");
   }
 }
