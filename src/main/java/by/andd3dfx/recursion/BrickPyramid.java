be02ebd4d2c9a5433adf045ed3cfedb7bf7ca413
Andrei Punko <andd3dfx@gmail.com>
2023-01-23 11:33:20 +0300

    Adjust BrickPyramid task

--- src/main/java/by/andd3dfx/recursion/BrickPyramid.java
+++ src/main/java/by/andd3dfx/recursion/BrickPyramid.java
@@ -1,54 +1,76 @@
 package by.andd3dfx.recursion;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * <pre>
+ * Кирпичная пирамида.
+ *
+ * Пирамида бесконечна. На верхнем слое лежит один кирпич весом 1кг, на втором слое - 2 кирпича по 1кг каждый,
+ * на третьем - три и т.д.
+ * Каждый кирпич равномерно давит на кирпичи под ним: 1/2 на левый кирпич и 1/2 на правый кирпич.
+ * Если на кирпич давит сверху общая масса 100кг, то давление также распределяется поровну на нижние кирпичи.
+ *
+ * Требуется:
+ * Реализовать функцию w(row, pos), которая вычисляет давление на кирпич на позиции row, pos.
+ * row - номер строки сверху, начиная с нуля,
+ * pos - номер кирпича в строке слева направо, начиная с нуля.
+ *
+ * Проверочные значения:
+ * w(0,0) = 0
+ *
+ * w(1,0) = 0.5
+ * w(0,1) = 0.5
+ *
+ * w(2,0) = 0.75
+ * w(2,1) = 1.5
+ * w(2,2) = 0.75
+ *
+ * w(3,0) = 0.875
+ * w(3,1) = 2.125
+ * w(3,2) = 2.125
+ * w(3,3) = 0.875
+ *
+ * w(322,156) = 306.48749781747574
+ * </pre>
+ */
 public class BrickPyramid {
 
-    private static final Map<Position, Double> positionToPressureMap = new HashMap<>();
+    private static final Map<Position, Double> cache = new HashMap<>() {{
+        put(new Position(0, 0), 0d);
+    }};
 
-    public static double pressure(int row, int pos) {
+    @Data
+    @RequiredArgsConstructor
+    public static class Position {
+        private final int row;
+        private final int pos;
+    }
+
+    public static double w(int row, int pos) {
         if (row < 0 || pos < 0 || row < pos) {
             throw new IllegalArgumentException("row and pos should satisfy conditions: row>=0, pos>=0, row>=pos");
         }
 
         Position position = new Position(row, pos);
-        if (positionToPressureMap.containsKey(position)) {
-            return positionToPressureMap.get(position);
+        if (cache.containsKey(position)) {
+            return cache.get(position);
         }
 
         double result = 0;
-        if (pos > 0) result += 0.5 * (pressure(row - 1, pos - 1) + 1);
-        if (pos < row) result += 0.5 * (pressure(row - 1, pos) + 1);
-        positionToPressureMap.put(position, result);
+        if (pos == 0) {
+            result += 0.5 * w(row - 1, pos) + 0.5;
+        } else if (pos == row) {
+            result += 0.5 * w(row - 1, pos - 1) + 0.5;
+        } else {
+            result += 0.5 * w(row - 1, pos - 1) + 0.5 * w(row - 1, pos) + 1;
+        }
+
+        cache.put(position, result);
         return result;
-    }
-
-    private static class Position {
-        private final int row;
-        private final int pos;
-
-        private Position(int row, int pos) {
-            this.row = row;
-            this.pos = pos;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-
-            Position position = (Position) o;
-
-            if (row != position.row) return false;
-            return pos == position.pos;
-        }
-
-        @Override
-        public int hashCode() {
-            int result = row;
-            result = 31 * result + pos;
-            return result;
-        }
     }
 }
--- src/test/java/by/andd3dfx/recursion/BrickPyramidTest.java
+++ src/test/java/by/andd3dfx/recursion/BrickPyramidTest.java
@@ -3,46 +3,43 @@ package by.andd3dfx.recursion;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
 public class BrickPyramidTest {
 
     @Test
-    public void weight() {
-        assertThat("w(0,0) = 0.0", BrickPyramid.pressure(0, 0), is(0.0));
+    public void w() {
+        assertThat(BrickPyramid.w(0, 0), is(0.0));
+        assertThat(BrickPyramid.w(1, 0), is(0.5));
+        assertThat(BrickPyramid.w(1, 1), is(0.5));
 
-        assertThat("w(1,0) = 0.5", BrickPyramid.pressure(1, 0), is(0.5));
-        assertThat("w(1,1) = 0.5", BrickPyramid.pressure(1, 1), is(0.5));
+        assertThat(BrickPyramid.w(2, 0), is(0.75));
+        assertThat(BrickPyramid.w(2, 1), is(1.5));
+        assertThat(BrickPyramid.w(2, 2), is(0.75));
 
-        assertThat("w(2,0) = 0.75", BrickPyramid.pressure(2, 0), is(0.75));
-        assertThat("w(2,1) = 1.5", BrickPyramid.pressure(2, 1), is(1.5));
-        assertThat("w(2,2) = 0.75", BrickPyramid.pressure(2, 2), is(0.75));
+        assertThat(BrickPyramid.w(3, 0), is(0.875));
+        assertThat(BrickPyramid.w(3, 1), is(2.125));
+        assertThat(BrickPyramid.w(3, 2), is(2.125));
+        assertThat(BrickPyramid.w(3, 3), is(0.875));
 
-        assertThat("w(3,0) = 0.875", BrickPyramid.pressure(3, 0), is(0.875));
-        assertThat("w(3,1) = 2.125", BrickPyramid.pressure(3, 1), is(2.125));
-        assertThat("w(3,2) = 2.125", BrickPyramid.pressure(3, 2), is(2.125));
-        assertThat("w(3,3) = 0.875", BrickPyramid.pressure(3, 3), is(0.875));
-
-        assertThat("w(322,156) = 306.48749781747574", BrickPyramid.pressure(322, 156),
-                is(306.48749781747574));
+        assertThat(BrickPyramid.w(322, 156), is(306.48749781747574));
     }
 
     @Test
-    public void weightWithWrongParams() {
-        checkWeightWithWrongParams(-1, 1);
-        checkWeightWithWrongParams(1, -1);
-        checkWeightWithWrongParams(-1, -1);
-        checkWeightWithWrongParams(2, 3);
+    public void wWithWrongParams() {
+        checkWExceptionPresenceWithWrongParams(-1, 1);
+        checkWExceptionPresenceWithWrongParams(1, -1);
+        checkWExceptionPresenceWithWrongParams(-1, -1);
+        checkWExceptionPresenceWithWrongParams(2, 3);
     }
 
-    private void checkWeightWithWrongParams(int row, int pos) {
+    private static void checkWExceptionPresenceWithWrongParams(int row, int pos) {
         try {
-            BrickPyramid.pressure(row, pos);
-            fail("IllegalArgumentException should be thrown");
+            BrickPyramid.w(row, pos);
+            fail("Exception should be thrown!");
         } catch (IllegalArgumentException iae) {
-            assertThat("Wrong exception message", iae.getMessage(),
-                    is("row and pos should satisfy conditions: row>=0, pos>=0, row>=pos"));
+            assertThat(iae.getMessage(), is("row and pos should satisfy conditions: row>=0, pos>=0, row>=pos"));
         }
     }
 }
