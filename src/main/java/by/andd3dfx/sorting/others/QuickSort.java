398b09b142f2bd392d591e7558b18b0bca8be68f
Andrei Punko <andd3dfx@gmail.com>
2023-02-11 16:13:55 +0300

    Add QuickSort implementation without parent class usage

---
+++ src/main/java/by/andd3dfx/sorting/others/QuickSort.java
@@ -0,0 +1,53 @@
+package by.andd3dfx.sorting.others;
+
+public class QuickSort {
+
+    public static <T extends Comparable> void apply(T[] items) {
+        quickSort(items, 0, items.length - 1);
+    }
+
+    private static <T extends Comparable> void quickSort(T[] items, int low, int high) {
+        if (low < high) {
+            int p = partition(items, low, high);
+            quickSort(items, low, p);
+            quickSort(items, p + 1, high);
+        }
+    }
+
+    private static <T extends Comparable> int partition(T[] items, int low, int high) {
+        var v = items[(low + high) / 2];
+        int i = low;
+        int j = high;
+
+        while (i <= j) {
+            while (lessThan(items[i], v)) {
+                i++;
+            }
+            while (greaterThan(items[j], v)) {
+                j--;
+            }
+            if (i >= j) {
+                break;
+            }
+            swap(items, i, j);
+            i++;
+            j--;
+        }
+
+        return j;
+    }
+
+    private static <T extends Comparable> boolean lessThan(T a, T b) {
+        return a.compareTo(b) < 0;
+    }
+
+    private static <T extends Comparable> boolean greaterThan(T a, T b) {
+        return a.compareTo(b) > 0;
+    }
+
+    private static <T> void swap(T[] items, int i, int j) {
+        var tmp = items[i];
+        items[i] = items[j];
+        items[j] = tmp;
+    }
+}
---
+++ src/test/java/by/andd3dfx/sorting/others/QuickSortTest.java
@@ -0,0 +1,35 @@
+package by.andd3dfx.sorting.others;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class QuickSortTest {
+
+    @Test
+    public void applyForEmpty() {
+        Integer[] array = {};
+
+        QuickSort.apply(array);
+
+        assertThat(array).isEqualTo(new Integer[]{});
+    }
+
+    @Test
+    public void applyForInt() {
+        Integer[] array = {4, 1, 6, 9, -2, 8, 5};
+
+        QuickSort.apply(array);
+
+        assertThat(array).isEqualTo(new Integer[]{-2, 1, 4, 5, 6, 8, 9});
+    }
+
+    @Test
+    public void applyForString() {
+        String[] array = {"Юлия", "Андрей", "Елена", "Павел", "Тихон"};
+
+        QuickSort.apply(array);
+
+        assertThat(array).isEqualTo(new String[]{"Андрей", "Елена", "Павел", "Тихон", "Юлия"});
+    }
+}
