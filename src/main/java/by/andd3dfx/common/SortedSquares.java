259c66f25217c2f40da246d7235f080eb416b7f1
Andrei Punko <andd3dfx@gmail.com>
2023-02-17 16:16:05 +0300

    Adjust SortedSquares task solution

--- src/main/java/by/andd3dfx/common/SortedSquares.java
+++ src/main/java/by/andd3dfx/common/SortedSquares.java
@@ -1,22 +1,64 @@
 package by.andd3dfx.common;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.List;
+import java.util.PriorityQueue;
 import java.util.stream.Collectors;
 
 /**
+ * <pre>
  * Дан массив целых чисел, отсортированный по возрастанию.
  * Вернуть массив, содержащий эл-ты исходного массива в квадрате, также отсортированный по возрастанию.
- * <p>
+ *
  * Пример:
  * [-5,-3,0,1,2,4] -> [0,1,4,9,16,25]
+ * </pre>
  */
 public class SortedSquares {
 
-    public static Integer[] transform(int[] arr) {
-        return Arrays.stream(arr)
-                .mapToObj(value -> value * value)
+    public static Integer[] transformUsingSorting(Integer[] items) {
+        return Arrays.stream(items)
+                .map(integer -> integer * integer)
                 .sorted()
                 .collect(Collectors.toList())
                 .toArray(new Integer[0]);
+    }
+
+    public static Integer[] transformUsingDeque(Integer[] items) {
+        int left = 0;
+        int right = items.length - 1;
+
+        Deque<Integer> queue = new ArrayDeque<>();
+        while (left <= right) {
+            var leftValue = items[left] * items[left];
+            var rightValue = items[right] * items[right];
+
+            if (leftValue < rightValue) {
+                queue.addFirst(rightValue);
+                right--;
+            } else {
+                queue.addFirst(leftValue);
+                left++;
+            }
+        }
+
+        return queue.toArray(new Integer[0]);
+    }
+
+    public static Integer[] transformUsingPriorityQueue(Integer[] items) {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Comparator.comparingInt(value -> value));
+        for (var item : items) {
+            priorityQueue.add(item * item);
+        }
+
+        List<Integer> result = new ArrayList<>();
+        while (!priorityQueue.isEmpty()) {
+            result.add(priorityQueue.poll());
+        }
+        return result.toArray(new Integer[0]);
     }
 }
--- src/test/java/by/andd3dfx/common/SortedSquaresTest.java
+++ src/test/java/by/andd3dfx/common/SortedSquaresTest.java
@@ -6,9 +6,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class SortedSquaresTest {
 
+    public static final Integer[] INITIAL_ARRAY = {-5, -3, 0, 1, 2, 4};
+    public static final Integer[] EXPECTED_ARRAY = {0, 1, 4, 9, 16, 25};
+
     @Test
-    public void transform() {
-        assertThat(SortedSquares.transform(new int[]{-5, -3, 0, 1, 2, 4}))
-                .isEqualTo(new Integer[]{0, 1, 4, 9, 16, 25});
+    public void transformUsingSorting() {
+        assertThat(SortedSquares.transformUsingSorting(INITIAL_ARRAY))
+                .isEqualTo(EXPECTED_ARRAY);
+    }
+
+    @Test
+    public void transformUsingDeque() {
+        assertThat(SortedSquares.transformUsingDeque(INITIAL_ARRAY))
+                .isEqualTo(EXPECTED_ARRAY);
+    }
+
+    @Test
+    public void transformUsingPriorityQueue() {
+        assertThat(SortedSquares.transformUsingPriorityQueue(INITIAL_ARRAY))
+                .isEqualTo(EXPECTED_ARRAY);
     }
 }
