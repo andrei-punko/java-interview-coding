52d32577bd52ebe3a061f6a0b4bf9360119a1624
Andrei Punko <andd3dfx@gmail.com>
2023-02-20 14:15:37 +0300

    Adjust SegmentIntersection task solution

--- src/test/java/by/andd3dfx/common/SegmentIntersectionTest.java
+++ src/test/java/by/andd3dfx/common/SegmentIntersectionTest.java
@@ -8,16 +8,18 @@ public class SegmentIntersectionTest {
 
     @Test
     public void find() {
-        assertThat(SegmentIntersection.find(new int[][]{{20, 30}, {19, 21}, {20, 26}, {29, 35}}))
-                .isEqualTo(new int[][]{{19, 21}, {29, 35}});
-
-        assertThat(SegmentIntersection.find(new int[][]{{20, 30}, {19, 21}, {20, 26}, {29, 35}, {10, 50}}))
-                .isEqualTo(new int[][]{{19, 21}, {29, 35}});
+        assertThat(SegmentIntersection.find(new int[][]{
+                {20, 30}, {19, 21}, {20, 26}, {29, 35}
+        })).isEqualTo(new int[][]{
+                {19, 21}, {29, 35}
+        });
     }
 
     @Test
     public void findWhenNothingFound() {
-        assertThat(SegmentIntersection.find(new int[][]{{20, 30}, {19, 21}, {20, 26}}))
-                .isEqualTo(new int[][]{});
+        assertThat(SegmentIntersection.find(new int[][]{
+                {20, 30}, {19, 21}, {20, 26}
+        })).isEqualTo(new int[][]{
+        });
     }
 }
