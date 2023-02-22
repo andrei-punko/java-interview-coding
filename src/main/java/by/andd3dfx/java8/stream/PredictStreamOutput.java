6200625f50249c3931ad3844e53f7f478bd2e7d9
Andrei Punko <andd3dfx@gmail.com>
2023-02-22 18:36:31 +0300

    Put all tasks "Predict stream output" into the same class

--- src/main/java/by/andd3dfx/java8/stream/PredictStreamOutput.java
+++ src/main/java/by/andd3dfx/java8/stream/PredictStreamOutput.java
@@ -1,22 +1,44 @@
 package by.andd3dfx.java8.stream;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class PredictStreamOutput {
 
     /**
-     * It should be: 123455
+     * Ответ:
+     * Saw 1
+     * Saw 4
+     * Saw 3
+     * Saw 2 1
+     * Saw 2 3
+     * <p>
+     * Cтрока "Saw 2 4" не выводится, т.к. на числе 3 выполнилось условие anyMatch() и дальнейший поиск не производится.
+     * Из javadoc к anyMatch():
+     * "Returns whether any elements of this stream match the provided predicate.
+     * May not evaluate the predicate on all elements if not necessary for determining the result."
      */
     public static void case1() {
+        Arrays.stream(new Integer[]{1, 4, 3})
+                .peek(e -> {
+                    System.out.println("Saw " + e);
+                })
+                .sorted()
+                .peek(e -> {
+                    System.out.println("Saw 2 " + e);
+                })
+                .anyMatch(item -> item == 3);
+    }
+
+    /**
+     * It will be: 123455
+     */
+    public static void case2() {
         List.of(1, 2, 3, 4, 5, 6, 7, 8, 9)
                 .stream()
                 .peek(i -> System.out.print(i))
                 .filter(i -> i > 4)
                 .peek(i -> System.out.print(i))
                 .findAny();
-    }
-
-    public static void main(String[] args) {
-        case1();
     }
 }
