a3368f1872dbf760604f3939e4532ee744b1b72d
Andrei Punko <andd3dfx@gmail.com>
2023-02-23 22:41:49 +0300

    Adjust FindTopNNumbers task solution

--- src/main/java/by/andd3dfx/sorting/FindTopNNumbers.java
+++ src/main/java/by/andd3dfx/sorting/FindTopNNumbers.java
@@ -3,29 +3,29 @@ package by.andd3dfx.sorting;
 import java.util.Arrays;
 
 /**
- * Find top ten numbers in array
+ * Find top k numbers in array
  */
 public class FindTopNNumbers {
 
-  /**
-   * We use short version of selection sort algorithm with O(kn) complexity
-   */
-  public static int[] find(int[] array, int k) {
-    for (int i = 0; i < k; i++) {
-      int maxElementIndex = i;
-      for (int j = i + 1; j < array.length; j++) {
-        if (array[j] > array[maxElementIndex]) {
-          maxElementIndex = j;
+    /**
+     * We use short version of selection sort algorithm with O(kn) complexity
+     */
+    public static int[] find(int[] array, int k) {
+        for (int i = 0; i < k; i++) {
+            int maxElementIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] > array[maxElementIndex]) {
+                    maxElementIndex = j;
+                }
+            }
+            swap(array, i, maxElementIndex);
         }
-      }
-      swap(array, i, maxElementIndex);
+        return Arrays.copyOf(array, k);
     }
-    return Arrays.copyOf(array, k);
-  }
 
-  private static void swap(int[] array, int i, int maxElementIndex) {
-    int temp = array[i];
-    array[i] = array[maxElementIndex];
-    array[maxElementIndex] = temp;
-  }
+    private static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 }
