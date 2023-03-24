31ba20887c19d54a69b547430801e995c0db2bcd
Andrei Punko <andd3dfx@gmail.com>
2023-03-24 16:52:39 +0300

    Add PasswordBruteforce task solution

---
+++ src/main/java/by/andd3dfx/common/PasswordBruteforce.java
@@ -0,0 +1,48 @@
+package by.andd3dfx.common;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.function.Function;
+
+/**
+ * We have alphabet of characters and hash function.
+ * Write decode() function which get hash value and restore password by this hash.
+ * Password contains only characters from given alphabet.
+ */
+public class PasswordBruteforce {
+
+    private final String alphabet;
+    private final Function<String, String> hashFunction;
+    private final String ZERO_CHARACTER = "ö";
+
+    public PasswordBruteforce(char[] alphabet, Function<String, String> hashFunction) {
+        this.alphabet = ZERO_CHARACTER + new String(alphabet);
+        this.hashFunction = hashFunction;
+    }
+
+    public String decode(String passwordHash, int maxPasswordLength) {
+        double maxNumber = Math.pow(alphabet.length(), maxPasswordLength);
+
+        int number = 1;
+        while (number < maxNumber) {
+            var password = encode(number);
+            String hash = hashFunction.apply(password);
+
+            if (StringUtils.equals(hash, passwordHash) && !password.contains(ZERO_CHARACTER)) {
+                return password;
+            }
+            number++;
+        }
+        return null;
+    }
+
+    String encode(int value) {
+        String result = "";
+        while (value > 0) {
+            var pos = value % alphabet.length();
+            result = alphabet.charAt(pos) + result;
+            value /= alphabet.length();
+        }
+        return result;
+    }
+}
---
+++ src/test/java/by/andd3dfx/common/PasswordBruteforceTest.java
@@ -0,0 +1,85 @@
+package by.andd3dfx.common;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Random;
+import java.util.function.Function;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+
+public class PasswordBruteforceTest {
+
+    private PasswordBruteforce passwordBruteforce;
+    private char[] alphabet;
+    private Function<String, String> hashFunction;
+
+    private static Random random = new Random();
+
+    @Before
+    public void setUp() throws Exception {
+        alphabet = new char[]{'P', 'u', 't', 'i', 'n'};
+        var alphabetStr = new String(alphabet);
+
+        hashFunction = s -> {
+            char[] chars = s.toCharArray();
+            var result = 0;
+            for (int i = 0; i < chars.length; i++) {
+                int index = alphabetStr.indexOf(chars[i]) + 1;
+                result = 17 * result + index;
+            }
+            return String.valueOf(result);
+        };
+
+        passwordBruteforce = new PasswordBruteforce(alphabet, hashFunction);
+    }
+
+    @Test
+    public void encode() {                                                 // öPutin -> 012345
+        assertThat(passwordBruteforce.encode(1), is("P"));      // 1 = 1*6^0 = 1 = P
+        assertThat(passwordBruteforce.encode(2), is("u"));      // 2 = 2*6^0 = 2 = u
+        assertThat(passwordBruteforce.encode(31), is("nP"));    // 31 = 5*6^1 + 1*6^0 = 30 + 1 = nP
+        assertThat(passwordBruteforce.encode(59), is("Ptn"));   // 59 = 1*6^2 + 3*6^1 + 5*6^0 = 36 + 18 + 5 = Ptn
+    }
+
+    @Test
+    public void decodeSimple() {
+        decodeUsingDefinitePassword("P");
+        decodeUsingDefinitePassword("PuPu");
+        decodeUsingDefinitePassword("unit");
+        decodeUsingDefinitePassword("nuiPin");
+    }
+
+    @Test
+    public void decodeRandomized() {
+        int times = 10;
+
+        for (var iteration = 0; iteration < times; iteration++) {
+            String password = generatePassword();
+            decodeUsingDefinitePassword(password);
+        }
+    }
+
+    @Test
+    public void decodeWhenHashContainsWrongCharacters() {
+        assertNull(passwordBruteforce.decode("1a", 4));
+    }
+
+    private String generatePassword() {
+        var passwordLength = 3 + random.nextInt(3);
+        char[] chars = new char[passwordLength];
+
+        for (int i = 0; i < passwordLength; i++) {
+            chars[i] = alphabet[random.nextInt(alphabet.length)];
+        }
+        return new String(chars);
+    }
+
+    private void decodeUsingDefinitePassword(String password) {
+        String hash = hashFunction.apply(password);
+        System.out.println("password=" + password + ", hash=" + hash);
+        assertThat(passwordBruteforce.decode(hash, password.length()), is(password));
+    }
+}
