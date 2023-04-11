bda1f5a87b7706a49c75c8046855743bd95564dd
Andrei Punko <andd3dfx@gmail.com>
2023-04-11 12:45:49 +0300

    Add solution for BestTimeToBuyNSellStock task

---
+++ src/main/java/by/andd3dfx/dynamic/BestTimeToBuyNSellStock.java
@@ -0,0 +1,52 @@
+package by.andd3dfx.dynamic;
+
+/**
+ * You are given an array prices where prices[i] is the price of a given stock on the ith day.
+ * <p>
+ * You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+ * <p>
+ * Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: prices = [7,1,5,3,6,4]
+ * Output: 5
+ * Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+ * Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: prices = [7,6,4,3,1]
+ * Output: 0
+ * Explanation: In this case, no transactions are done and the max profit = 0.
+ * <p>
+ * https://leetcode.com/explore/interview/card/top-interview-questions-easy/97/dynamic-programming/572/
+ */
+public class BestTimeToBuyNSellStock {
+
+    public int maxProfit_ON2(int[] prices) {
+        var maxProfit = 0;
+        for (int indexToBuy = 0; indexToBuy < prices.length - 1; indexToBuy++) {
+            for (int indexToSell = indexToBuy + 1; indexToSell < prices.length; indexToSell++) {
+                var profit = prices[indexToSell] - prices[indexToBuy];
+                if (profit > maxProfit) {
+                    maxProfit = profit;
+                }
+            }
+        }
+        return maxProfit;
+    }
+
+    public int maxProfit_ON(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+}
---
+++ src/test/java/by/andd3dfx/dynamic/BestTimeToBuyNSellStockTest.java
@@ -0,0 +1,37 @@
+package by.andd3dfx.dynamic;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class BestTimeToBuyNSellStockTest {
+
+    private BestTimeToBuyNSellStock solver;
+
+    @Before
+    public void setUp() throws Exception {
+        solver = new BestTimeToBuyNSellStock();
+    }
+
+    @Test
+    public void maxProfit_ON2() {
+        assertThat(solver.maxProfit_ON2(new int[]{1, 2})).isEqualTo(1);
+        assertThat(solver.maxProfit_ON2(new int[]{2, 2})).isEqualTo(0);
+        assertThat(solver.maxProfit_ON2(new int[]{2, 1, 2, 0, 1})).isEqualTo(1);
+        assertThat(solver.maxProfit_ON2(new int[]{2, 1, 2, 0, 1, 2})).isEqualTo(2);
+        assertThat(solver.maxProfit_ON2(new int[]{7, 1, 5, 3, 6, 4})).isEqualTo(5);
+        assertThat(solver.maxProfit_ON2(new int[]{7, 6, 4, 3, 1})).isEqualTo(0);
+    }
+
+    @Test
+    public void maxProfit_ON() {
+        assertThat(solver.maxProfit_ON(new int[]{1, 2})).isEqualTo(1);
+        assertThat(solver.maxProfit_ON(new int[]{2, 2})).isEqualTo(0);
+        assertThat(solver.maxProfit_ON(new int[]{2, 1, 2, 0, 1})).isEqualTo(1);
+        assertThat(solver.maxProfit_ON(new int[]{2, 1, 2, 0, 1, 2})).isEqualTo(2);
+        assertThat(solver.maxProfit_ON(new int[]{7, 1, 5, 3, 6, 4})).isEqualTo(5);
+        assertThat(solver.maxProfit_ON(new int[]{7, 6, 4, 3, 1})).isEqualTo(0);
+    }
+}
