bf1215aed840c6afe2ce0b0ce9559adb7b46a4de
Andrei Punko <andd3dfx@gmail.com>
2023-05-08 12:53:02 +0300

    Adjust CustomLinkedList: add methods and tests

--- src/main/java/by/andd3dfx/collections/CustomLinkedList.java
+++ src/main/java/by/andd3dfx/collections/CustomLinkedList.java
@@ -1,7 +1,6 @@
 package by.andd3dfx.collections;
 
 import lombok.AllArgsConstructor;
-import lombok.ToString;
 
 public class CustomLinkedList<T> {
 
@@ -12,41 +11,48 @@ public class CustomLinkedList<T> {
 
         @Override
         public String toString() {
+            if (next == null) {
+                return "{%s}".formatted(value);
+            }
             return "{%s, next=%s}".formatted(value, next);
         }
     }
 
     private Node head;
-    private Node tail;
     private int size = 0;
 
-    public void reverse() {
-        Node prev = null;
-        Node curr = head;
-        tail = head;
-
-        while (curr != null) {
-            Node next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        head = prev;
+    public void add(T value) {
+        add(size, value);
     }
 
-    public void add(T value) {
-        Node node = new Node(value, null);
-        if (size == 0) {
-            head = tail = node;
-        } else {
-            tail.next = node;
-            tail = node;
+    private void add(int index, T value) {
+        if (index == 0) {
+            var newNode = new Node(value, head);
+            head = newNode;
+            size++;
+            return;
         }
+
+        int i = 1;
+        Node prev = head;
+        Node curr = head.next;
+        while (i < index) {
+            i++;
+            prev = curr;
+            curr = curr.next;
+        }
+        var newNode = new Node(value, curr);
+        prev.next = newNode;
+        newNode.next = (curr == null) ? null : curr.next;
         size++;
     }
 
     public int size() {
         return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
     }
 
     public T get(int index) {
@@ -61,6 +67,71 @@ public class CustomLinkedList<T> {
             curr = curr.next;
         }
         return curr.value;
+    }
+
+    public T remove() {
+        return remove(0);
+    }
+
+    public T remove(int index) {
+        if (index >= size) {
+            throw new IndexOutOfBoundsException(String.format("Wrong index: %d", index));
+        }
+
+        if (index == 0) {
+            T result = head.value;
+            head = head.next;
+            size--;
+            return result;
+        }
+
+        int i = 1;
+        Node prev = head;
+        Node curr = head.next;
+        while (i < index) {
+            i++;
+            prev = curr;
+            curr = curr.next;
+        }
+        prev.next = curr.next;
+        size--;
+
+        return curr.value;
+    }
+
+    public boolean remove(T value) {
+        var i = 0;
+        var curr = head;
+        while (curr != null) {
+            if ((value == null && curr.value == null) || value.equals(curr.value)) {
+                remove(i);
+                return true;
+            }
+            i++;
+            curr = curr.next;
+        }
+        return false;
+    }
+
+    public void push(T item) {
+        add(0, item);
+    }
+
+    public T pop() {
+        return remove(0);
+    }
+
+    public void reverse() {
+        Node prev = null;
+        Node curr = head;
+
+        while (curr != null) {
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
     }
 
     @Override
--- src/test/java/by/andd3dfx/collections/CustomLinkedListTest.java
+++ src/test/java/by/andd3dfx/collections/CustomLinkedListTest.java
@@ -4,8 +4,35 @@ import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class CustomLinkedListTest {
+
+    @Test
+    public void addNGet() {
+        CustomLinkedList<Integer> linkedList = new CustomLinkedList<>();
+        assertThat(linkedList.size(), is(0));
+        assertTrue(linkedList.isEmpty());
+
+        linkedList.add(3);
+        linkedList.add(7);
+
+        assertThat(linkedList.size(), is(2));
+        assertFalse(linkedList.isEmpty());
+        assertThat(linkedList.get(0), is(3));
+        assertThat(linkedList.get(1), is(7));
+
+        linkedList.add(12);
+        linkedList.add(34);
+
+        assertThat(linkedList.size(), is(4));
+        assertFalse(linkedList.isEmpty());
+        assertThat(linkedList.get(0), is(3));
+        assertThat(linkedList.get(1), is(7));
+        assertThat(linkedList.get(2), is(12));
+        assertThat(linkedList.get(3), is(34));
+    }
 
     @Test
     public void reverse() {
@@ -15,18 +42,163 @@ public class CustomLinkedListTest {
         linkedList.add(12);
         linkedList.add(34);
 
-        assertThat("Wrong size", linkedList.size(), is(4));
-        assertThat("Wrong list[0] item", linkedList.get(0), is(3));
-        assertThat("Wrong list[1] item", linkedList.get(1), is(7));
-        assertThat("Wrong list[2] item", linkedList.get(2), is(12));
-        assertThat("Wrong list[3] item", linkedList.get(3), is(34));
+        assertThat(linkedList.size(), is(4));
+        assertThat(linkedList.get(0), is(3));
+        assertThat(linkedList.get(1), is(7));
+        assertThat(linkedList.get(2), is(12));
+        assertThat(linkedList.get(3), is(34));
 
         linkedList.reverse();
 
-        assertThat("Wrong size", linkedList.size(), is(4));
-        assertThat("Wrong list[0] item", linkedList.get(0), is(34));
-        assertThat("Wrong list[1] item", linkedList.get(1), is(12));
-        assertThat("Wrong list[2] item", linkedList.get(2), is(7));
-        assertThat("Wrong list[3] item", linkedList.get(3), is(3));
+        assertThat(linkedList.size(), is(4));
+        assertThat(linkedList.get(0), is(34));
+        assertThat(linkedList.get(1), is(12));
+        assertThat(linkedList.get(2), is(7));
+        assertThat(linkedList.get(3), is(3));
+    }
+
+    @Test
+    public void remove() {
+        CustomLinkedList<Integer> linkedList = new CustomLinkedList<>();
+        linkedList.add(11);
+        linkedList.add(7);
+        linkedList.add(12);
+        linkedList.add(34);
+
+        var removed = linkedList.remove();
+
+        assertThat(removed, is(11));
+        assertThat(linkedList.size(), is(3));
+        assertThat(linkedList.get(0), is(7));
+        assertThat(linkedList.get(1), is(12));
+        assertThat(linkedList.get(2), is(34));
+
+        var removed2 = linkedList.remove();
+
+        assertThat(removed2, is(7));
+        assertThat(linkedList.size(), is(2));
+        assertThat(linkedList.get(0), is(12));
+        assertThat(linkedList.get(1), is(34));
+    }
+
+    @Test
+    public void removeByIndex() {
+        CustomLinkedList<Integer> linkedList = new CustomLinkedList<>();
+        linkedList.add(11);
+        linkedList.add(7);
+        linkedList.add(12);
+        linkedList.add(34);
+
+        var removed = linkedList.remove(2);
+
+        assertThat(removed, is(12));
+        assertThat(linkedList.size(), is(3));
+        assertThat(linkedList.get(0), is(11));
+        assertThat(linkedList.get(1), is(7));
+        assertThat(linkedList.get(2), is(34));
+
+        var removed2 = linkedList.remove(2);
+
+        assertThat(removed2, is(34));
+        assertThat(linkedList.size(), is(2));
+        assertThat(linkedList.get(0), is(11));
+        assertThat(linkedList.get(1), is(7));
+    }
+
+    @Test
+    public void removeByValue() {
+        CustomLinkedList<String> linkedList = new CustomLinkedList<>();
+        linkedList.add("Andrei");
+        linkedList.add("Tikhon");
+        linkedList.add("Ilya");
+        linkedList.add("Elena");
+        linkedList.add("Yulia");
+
+        var removeResult = linkedList.remove("Ilya");
+
+        assertTrue(removeResult);
+        assertThat(linkedList.size(), is(4));
+        assertThat(linkedList.get(0), is("Andrei"));
+        assertThat(linkedList.get(1), is("Tikhon"));
+        assertThat(linkedList.get(2), is("Elena"));
+        assertThat(linkedList.get(3), is("Yulia"));
+
+        var removeResult2 = linkedList.remove("John");
+
+        assertFalse(removeResult2);
+        assertThat(linkedList.size(), is(4));
+        assertThat(linkedList.get(0), is("Andrei"));
+        assertThat(linkedList.get(1), is("Tikhon"));
+        assertThat(linkedList.get(2), is("Elena"));
+        assertThat(linkedList.get(3), is("Yulia"));
+    }
+
+    @Test
+    public void removeByValueWhenAbsent() {
+        CustomLinkedList<String> linkedList = new CustomLinkedList<>();
+        linkedList.add("Andrei");
+        linkedList.add("Tikhon");
+        linkedList.add("Ilya");
+        linkedList.add("Elena");
+        linkedList.add("Yulia");
+
+        var removed = linkedList.remove("Oksana");
+
+        assertFalse(removed);
+        assertThat(linkedList.size(), is(5));
+        assertThat(linkedList.get(0), is("Andrei"));
+        assertThat(linkedList.get(1), is("Tikhon"));
+        assertThat(linkedList.get(2), is("Ilya"));
+        assertThat(linkedList.get(3), is("Elena"));
+        assertThat(linkedList.get(4), is("Yulia"));
+    }
+
+    @Test
+    public void pushNPop() {
+        CustomLinkedList<Integer> linkedList = new CustomLinkedList<>();
+        assertTrue(linkedList.isEmpty());
+        linkedList.push(2);
+        linkedList.push(3);
+        linkedList.push(36);
+        linkedList.push(39);
+
+        assertThat(linkedList.pop(), is(39));
+        assertThat(linkedList.pop(), is(36));
+        assertThat(linkedList.pop(), is(3));
+        assertFalse(linkedList.isEmpty());
+        assertThat(linkedList.pop(), is(2));
+        assertTrue(linkedList.isEmpty());
+    }
+
+    @Test
+    public void pushNPopNRemove() {
+        CustomLinkedList<Integer> linkedList = new CustomLinkedList<>();
+        linkedList.push(2);
+        linkedList.push(3);
+        linkedList.push(36);    // 36 3 2
+        assertThat(linkedList.remove(1), is(3));   // 36 2
+        linkedList.push(39);
+        linkedList.push(43);
+        linkedList.push(45);    // 45 43 39 36 2
+        assertThat(linkedList.remove(2), is(39));   // 45 43 36 2
+
+        assertThat(linkedList.pop(), is(45));
+        assertThat(linkedList.pop(), is(43));
+        assertThat(linkedList.pop(), is(36));
+        assertThat(linkedList.pop(), is(2));
+        assertTrue(linkedList.isEmpty());
+    }
+
+    @Test
+    public void testToString() {
+        CustomLinkedList<Integer> linkedList = new CustomLinkedList<>();
+        linkedList.add(3);
+        linkedList.add(7);
+        linkedList.add(12);
+        linkedList.add(34);
+
+        var result = linkedList.toString();
+
+        assertThat(result, is("CustomLinkedList{head={3, next={7, next={12, next={34}}}}}"));
     }
 }
