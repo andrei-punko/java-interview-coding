93ddf14fe46d4d595b9f5909954af0263e90b490
Andrei Punko <andd3dfx@gmail.com>
2023-05-08 23:03:29 +0300

    Add CustomArrayList implementation and tests

---
+++ src/main/java/by/andd3dfx/collections/custom/CustomArrayList.java
@@ -0,0 +1,113 @@
+package by.andd3dfx.collections.custom;
+
+import java.util.Arrays;
+
+public class CustomArrayList<T> {
+
+    private static final int DEFAULT_INITIAL_SIZE = 10;
+    private static final float RESIZE_FACTOR = 1.5f;
+    private static final float INVERSE_RESIZE_FACTOR = 1 / RESIZE_FACTOR;
+
+    private T[] array;
+    private int size = 0;
+
+    public CustomArrayList() {
+        this(DEFAULT_INITIAL_SIZE);
+    }
+
+    public CustomArrayList(int length) {
+        array = (T[]) new Object[length];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void set(int index, T value) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException(String.format("Wrong index: %d", index));
+        }
+
+        array[index] = value;
+    }
+
+    public void add(T value) {
+        add(size, value);
+    }
+
+    public void add(int index, T value) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (size == array.length) {
+            int oldLength = array.length;
+            int newLength = Math.round(RESIZE_FACTOR * oldLength);
+            var newArray = (T[]) new Object[newLength];
+            System.arraycopy(array, 0, newArray, 0, size);
+            array = newArray;
+
+            System.out.println("Inner array resized: %s->%s".formatted(oldLength, newLength));
+        }
+
+        if (index < size) {
+            System.arraycopy(array, index, array, index + 1, size - index);
+        }
+        array[index] = value;
+        size++;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException(String.format("Wrong index: %d", index));
+        }
+
+        return array[index];
+    }
+
+    public T remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException(String.format("Wrong index: %d", index));
+        }
+
+        var result = array[index];
+        if (index < size) {
+            System.arraycopy(array, index + 1, array, index, size - index);
+        }
+        size--;
+
+        if (array.length > DEFAULT_INITIAL_SIZE && size < array.length / 2) {
+            int oldLength = array.length;
+            int newLength = Math.round(INVERSE_RESIZE_FACTOR * oldLength);
+            var newArray = (T[]) new Object[newLength];
+            System.arraycopy(array, 0, newArray, 0, size);
+            array = newArray;
+
+            System.out.println("Inner array resized: %s->%s".formatted(oldLength, newLength));
+        }
+
+        return result;
+    }
+
+    public boolean remove(T value) {
+        var i = 0;
+        while (i < array.length) {
+            T currValue = array[i];
+            if ((value == null && currValue == null) || value.equals(currValue)) {
+                remove(i);
+                return true;
+            }
+            i++;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(Arrays.copyOf(array, size));
+    }
+}
---
+++ src/test/java/by/andd3dfx/collections/custom/CustomArrayListTest.java
@@ -0,0 +1,211 @@
+package by.andd3dfx.collections.custom;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+
+public class CustomArrayListTest {
+
+    @Test
+    public void addNGet() {
+        CustomArrayList<Integer> list = new CustomArrayList<>();
+        assertThat(list.size(), is(0));
+        assertTrue(list.isEmpty());
+        list.add(4);
+        list.add(5);
+        list.add(12);
+
+        assertThat(list.size(), is(3));
+        assertFalse(list.isEmpty());
+        assertThat(list.get(0), is(4));
+        assertThat(list.get(1), is(5));
+        assertThat(list.get(2), is(12));
+    }
+
+    @Test
+    public void addNGetWhenArrayResizeExpected() {
+        CustomArrayList<Integer> list = new CustomArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            list.add(i * i);
+        }
+
+        assertThat(list.size(), is(20));
+        assertFalse(list.isEmpty());
+        for (int i = 0; i < 20; i++) {
+            assertThat(list.get(i), is(i * i));
+        }
+    }
+
+    @Test
+    public void addIntoMiddleOfListNGetWhenArrayResizeExpected() {
+        CustomArrayList<Integer> list = new CustomArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(i * i);
+        }
+        for (int i = 0; i < 10; i++) {
+            list.add(i + 5, i * i);
+        }
+
+        assertThat(list.size(), is(20));
+        assertFalse(list.isEmpty());
+        for (int i = 0; i < 5; i++) {
+            assertThat(list.get(i), is(i * i));
+        }
+        for (int i = 0; i < 10; i++) {
+            assertThat(list.get(i + 5), is(i * i));
+        }
+        for (int i = 5; i < 10; i++) {
+            assertThat(list.get(i + 10), is(i * i));
+        }
+    }
+
+    @Test
+    public void addNSetNGet() {
+        CustomArrayList<Integer> list = new CustomArrayList<>();
+        assertThat(list.size(), is(0));
+        assertTrue(list.isEmpty());
+        list.add(4);
+        list.add(5);
+        list.set(1, 56);
+        list.add(12);
+        list.set(0, 31);
+
+        assertThat(list.size(), is(3));
+        assertFalse(list.isEmpty());
+        assertThat(list.get(0), is(31));
+        assertThat(list.get(1), is(56));
+        assertThat(list.get(2), is(12));
+    }
+
+    @Test
+    public void addByIndexWhenOutOfRange() {
+        CustomArrayList<Integer> list = new CustomArrayList<>();
+        list.add(3);
+        list.add(7);
+        list.add(12);
+
+        assertThrows(IndexOutOfBoundsException.class, () -> list.add(-1, 34));
+        assertThrows(IndexOutOfBoundsException.class, () -> list.add(4, 34));
+    }
+
+    @Test
+    public void getByIndexWhenOutOfRange() {
+        CustomArrayList<Integer> list = new CustomArrayList<>();
+        list.add(3);
+        list.add(7);
+        list.add(12);
+
+        assertThrows(IndexOutOfBoundsException.class, () -> list.get(-1));
+        assertThrows(IndexOutOfBoundsException.class, () -> list.get(3));
+        assertThrows(IndexOutOfBoundsException.class, () -> list.get(5));
+    }
+
+    @Test
+    public void setByIndexWhenOutOfRange() {
+        CustomArrayList<Integer> list = new CustomArrayList<>();
+        list.add(3);
+        list.add(7);
+        list.add(12);
+
+        assertThrows(IndexOutOfBoundsException.class, () -> list.set(-1, 34));
+        assertThrows(IndexOutOfBoundsException.class, () -> list.set(4, 34));
+    }
+
+    @Test
+    public void removeByIndex() {
+        CustomArrayList<Integer> list = new CustomArrayList<>();
+        list.add(11);
+        list.add(7);
+        list.add(12);
+        list.add(34);
+
+        var removed = list.remove(2);
+
+        assertThat(removed, is(12));
+        assertThat(list.size(), is(3));
+        assertThat(list.get(0), is(11));
+        assertThat(list.get(1), is(7));
+        assertThat(list.get(2), is(34));
+
+        var removed2 = list.remove(2);
+
+        assertThat(removed2, is(34));
+        assertThat(list.size(), is(2));
+        assertThat(list.get(0), is(11));
+        assertThat(list.get(1), is(7));
+    }
+
+    @Test
+    public void removeByIndexWhenOutOfRange() {
+        CustomArrayList<Integer> list = new CustomArrayList<>();
+        list.add(3);
+        list.add(7);
+        list.add(12);
+
+        assertThrows(IndexOutOfBoundsException.class, () -> list.remove(-1));
+        assertThrows(IndexOutOfBoundsException.class, () -> list.remove(3));
+        assertThrows(IndexOutOfBoundsException.class, () -> list.remove(5));
+    }
+
+    @Test
+    public void removeByValue() {
+        CustomArrayList<String> list = new CustomArrayList<>();
+        list.add("Andrei");
+        list.add("Tikhon");
+        list.add("Ilya");
+        list.add("Elena");
+        list.add("Yulia");
+
+        var removeResult = list.remove("Ilya");
+
+        assertTrue(removeResult);
+        assertThat(list.size(), is(4));
+        assertThat(list.get(0), is("Andrei"));
+        assertThat(list.get(1), is("Tikhon"));
+        assertThat(list.get(2), is("Elena"));
+        assertThat(list.get(3), is("Yulia"));
+
+        var removeResult2 = list.remove("John");
+
+        assertFalse(removeResult2);
+        assertThat(list.size(), is(4));
+        assertThat(list.get(0), is("Andrei"));
+        assertThat(list.get(1), is("Tikhon"));
+        assertThat(list.get(2), is("Elena"));
+        assertThat(list.get(3), is("Yulia"));
+    }
+
+    @Test
+    public void removeByValueWhenAbsent() {
+        CustomArrayList<String> list = new CustomArrayList<>();
+        list.add("Andrei");
+        list.add("Tikhon");
+        list.add("Ilya");
+        list.add("Elena");
+        list.add("Yulia");
+
+        var removed = list.remove("Oksana");
+
+        assertFalse(removed);
+        assertThat(list.size(), is(5));
+        assertThat(list.get(0), is("Andrei"));
+        assertThat(list.get(1), is("Tikhon"));
+        assertThat(list.get(2), is("Ilya"));
+        assertThat(list.get(3), is("Elena"));
+        assertThat(list.get(4), is("Yulia"));
+    }
+
+    @Test
+    public void testToString() {
+        CustomArrayList<String> list = new CustomArrayList<>();
+        list.add("Andrei");
+        list.add("Tikhon");
+        list.add("Ilya");
+
+        assertThat(list.toString(), is("[Andrei, Tikhon, Ilya]"));
+    }
+}
