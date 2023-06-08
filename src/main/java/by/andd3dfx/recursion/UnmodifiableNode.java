6d9eba6a299189214496260078e05b5a54f9920e
Andrei Punko <andd3dfx@gmail.com>
2023-06-09 00:01:48 +0300

    Adjust Immutable Node task solution and test

---
+++ src/main/java/by/andd3dfx/recursion/UnmodifiableNode.java
@@ -0,0 +1,96 @@
+package by.andd3dfx.recursion;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * 1. Modify given class Node to make it immutable:
+ *
+ * <pre>
+ * public final class Node {
+ *     private final List<Node> children;
+ *     private final Integer value;
+ * }
+ * </pre>
+ * <p>
+ * 2. Provide name of this data structure
+ * <p>
+ * 3. Add sum() method to sum values of all items in this structure
+ * <ul>
+ *     <li>recursive way
+ *     <li>non-recursive way
+ * </ul>
+ */
+public final class UnmodifiableNode {
+
+    private final List<UnmodifiableNode> children = new ArrayList<>();
+    private final Integer value;
+
+    public UnmodifiableNode(Integer value) {
+        this.value = value;
+    }
+
+    public UnmodifiableNode(List<UnmodifiableNode> children, Integer value) {
+        if (children != null) {
+            this.children.addAll(children);
+        }
+        this.value = value;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public List<UnmodifiableNode> getChildren() {
+        return new ArrayList(children);
+    }
+
+    public int sumRecursive() {
+        return sum(this);
+    }
+
+    private int sum(UnmodifiableNode node) {
+        var result = node.value;
+        for (UnmodifiableNode child : node.getChildren()) {
+            result += sum(child);
+        }
+        return result;
+    }
+
+    public int sumRecursive2() {
+        return flattennedStream()
+                .map(UnmodifiableNode::getValue)
+                .reduce(0, (a, b) -> a + b);
+    }
+
+    private Stream<UnmodifiableNode> flattennedStream() {
+        return Stream.concat(
+                Stream.of(this),
+                children.stream().flatMap(UnmodifiableNode::flattennedStream)
+        );
+    }
+
+    public int sumNonRecursive() {
+        var result = value;
+        var stack = new ArrayDeque<List<UnmodifiableNode>>();
+        stack.add(children);
+
+        while (!stack.isEmpty()) {
+            for (var node : stack.pop()) {
+                result += node.value;
+                stack.add(node.children);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        if (children.isEmpty()) {
+            return "{%d}".formatted(value);
+        }
+        return "{%d, %s}".formatted(value, children);
+    }
+}
---
+++ src/test/java/by/andd3dfx/recursion/UnmodifiableNodeTest.java
@@ -0,0 +1,60 @@
+package by.andd3dfx.recursion;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UnmodifiableNodeTest {
+
+    @Test
+    public void sumRecursiveForPlainNode() {
+        UnmodifiableNode node = new UnmodifiableNode(12);
+        assertThat(node.sumRecursive()).isEqualTo(12);
+    }
+
+    @Test
+    public void sumRecursiveForComplexNode() {
+        UnmodifiableNode node = prepareComplexNode();
+        assertThat(node.sumRecursive()).isEqualTo(60);
+    }
+
+    @Test
+    public void sumRecursive2ForPlainNode() {
+        UnmodifiableNode node = new UnmodifiableNode(12);
+        assertThat(node.sumRecursive2()).isEqualTo(12);
+    }
+
+    @Test
+    public void sumRecursive2ForComplexNode() {
+        UnmodifiableNode node = prepareComplexNode();
+        assertThat(node.sumRecursive2()).isEqualTo(60);
+    }
+
+    @Test
+    public void sumNonRecursiveForPlainNode() {
+        UnmodifiableNode node = new UnmodifiableNode(12);
+        assertThat(node.sumNonRecursive()).isEqualTo(12);
+    }
+
+    @Test
+    public void sumNonRecursiveForComplexNode() {
+        UnmodifiableNode node = prepareComplexNode();
+        assertThat(node.sumNonRecursive()).isEqualTo(60);
+    }
+
+    @Test
+    public void testToString() {
+        UnmodifiableNode node = prepareComplexNode();
+        assertThat(node.toString()).isEqualTo("{12, [{2}, {3, [{32}, {11}]}]}");
+    }
+
+    private UnmodifiableNode prepareComplexNode() {
+        return new UnmodifiableNode(Arrays.asList(
+                new UnmodifiableNode(2),
+                new UnmodifiableNode(List.of(new UnmodifiableNode(32), new UnmodifiableNode(11)), 3)
+        ), 12);
+    }
+}
