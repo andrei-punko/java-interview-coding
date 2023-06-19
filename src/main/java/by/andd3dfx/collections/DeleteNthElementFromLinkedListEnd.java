9f50285c32d19c4438ec288eff585f79194306e6
Andrei Punko <andd3dfx@gmail.com>
2023-06-19 20:02:32 +0300

    Add solution for DeleteNthElementFromLinkedListEnd task

---
+++ src/main/java/by/andd3dfx/collections/DeleteNthElementFromLinkedListEnd.java
@@ -0,0 +1,47 @@
+package by.andd3dfx.collections;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+public class DeleteNthElementFromLinkedListEnd {
+
+    @Data
+    @AllArgsConstructor
+    public static class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "{" + value + ", next=" + next + '}';
+        }
+    }
+
+    public static void apply(Node head, int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("n should be greater than 0!");
+        }
+
+        int counter = n;
+        var fastPointer = head;
+        while (counter >= 0) {
+            if (fastPointer == null) {
+                throw new IllegalArgumentException("n should be less than list size!");
+            }
+            fastPointer = fastPointer.next;
+            counter--;
+        }
+
+        var slowPointer = head;
+        while (fastPointer.next != null) {
+            fastPointer = fastPointer.next;
+            slowPointer = slowPointer.next;
+        }
+
+        slowPointer.next = slowPointer.next.next;
+    }
+}
---
+++ src/test/java/by/andd3dfx/collections/DeleteNthElementFromLinkedListEndTest.java
@@ -0,0 +1,56 @@
+package by.andd3dfx.collections;
+
+import by.andd3dfx.collections.DeleteNthElementFromLinkedListEnd.Node;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertThrows;
+
+public class DeleteNthElementFromLinkedListEndTest {
+
+    @Test
+    public void applyFor0() {
+        Node head = buildLinkedList();
+
+        DeleteNthElementFromLinkedListEnd.apply(head, 0);
+
+        assertThat(head.getValue()).isEqualTo(10);
+        assertThat(head.getNext().getValue()).isEqualTo(11);
+        assertThat(head.getNext().getNext().getValue()).isEqualTo(12);
+        assertThat(head.getNext().getNext().getNext().getValue()).isEqualTo(13);
+        assertThat(head.getNext().getNext().getNext().getNext()).isNull();
+    }
+
+    @Test
+    public void applyFor2() {
+        Node head = buildLinkedList();
+
+        DeleteNthElementFromLinkedListEnd.apply(head, 2);
+
+        assertThat(head.getValue()).isEqualTo(10);
+        assertThat(head.getNext().getValue()).isEqualTo(11);
+        assertThat(head.getNext().getNext().getValue()).isEqualTo(13);
+        assertThat(head.getNext().getNext().getNext().getValue()).isEqualTo(14);
+        assertThat(head.getNext().getNext().getNext().getNext()).isNull();
+    }
+
+    @Test
+    public void applyWhenNIsNegative() {
+        Node head = buildLinkedList();
+
+        assertThrows("n should be greater than 0!", IllegalArgumentException.class,
+                () -> DeleteNthElementFromLinkedListEnd.apply(head, -1));
+    }
+
+    @Test
+    public void applyWhenNGreaterThanListSize() {
+        Node head = buildLinkedList();
+
+        assertThrows("n should be less than list size!", IllegalArgumentException.class,
+                () -> DeleteNthElementFromLinkedListEnd.apply(head, 5));
+    }
+
+    private Node buildLinkedList() {
+        return new Node(10, new Node(11, new Node(12, new Node(13, new Node(14)))));
+    }
+}
