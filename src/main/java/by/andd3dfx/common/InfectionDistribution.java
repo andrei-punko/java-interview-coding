d2f03e71d05ac1d305266ae899f61b525d23825d
Andrei Punko <andd3dfx@gmail.com>
2023-07-10 18:58:16 +0300

    Enhance and add one more solution for InfectionDistribution task

--- src/main/java/by/andd3dfx/common/InfectionDistribution.java
+++ src/main/java/by/andd3dfx/common/InfectionDistribution.java
@@ -1,11 +1,13 @@
 package by.andd3dfx.common;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * <pre>
@@ -25,14 +27,18 @@ import java.util.Set;
  */
 public class InfectionDistribution {
 
-    public static int weeksToInfectAllCities(int citiesAmount, int[][] roads, int[] infected) {
+    /**
+     * Initial solution
+     */
+    public static int weeksToInfectAllCities_usingIterationByRoads(int citiesAmount, int[][] roads, int[] infected) {
         int weeksCount = 0;
-        Set<Integer> infectedCitiesSet = new HashSet<>();
-        Arrays.stream(infected).forEach(number -> infectedCitiesSet.add(number));
+        var infectedCitiesSet = Arrays.stream(infected).boxed()
+                .collect(Collectors.toSet());
         var roadsSet = buildRoadsSet(roads);
 
         while (!isAllInfected(citiesAmount, infectedCitiesSet)) {
             var infectedOnThisIteration = processOneWeek(roadsSet, infectedCitiesSet);
+
             if (infectedOnThisIteration == 0) {
                 throw new IllegalArgumentException("Could not infect all cities!");
             }
@@ -42,22 +48,19 @@ public class InfectionDistribution {
         return weeksCount;
     }
 
-    @Data
-    @AllArgsConstructor
-    private static class Road {
-        private int start;
-        private int end;
+    private static boolean isAllInfected(int citiesAmount, Set<Integer> infectedCitiesSet) {
+        return infectedCitiesSet.size() == citiesAmount;
     }
 
     private static Set<Road> buildRoadsSet(int[][] roads) {
-        var result = new HashSet<Road>();
-        Arrays.stream(roads)
-                .forEach(road -> result.add(new Road(road[0], road[1])));
-        return result;
+        return Arrays.stream(roads)
+                .map(Road::new)
+                .collect(Collectors.toSet());
     }
 
     private static int processOneWeek(Set<Road> roads, Set<Integer> infectedCitiesSet) {
         var initialCitiesSet = new HashSet<>(infectedCitiesSet);
+
         var it = roads.iterator();
         while (it.hasNext()) {
             var road = it.next();
@@ -73,10 +76,69 @@ public class InfectionDistribution {
                 it.remove();
             }
         }
+
         return infectedCitiesSet.size() - initialCitiesSet.size();
     }
 
-    private static boolean isAllInfected(int citiesAmount, Set<Integer> infectedCitiesSet) {
-        return infectedCitiesSet.size() == citiesAmount;
+    @Data
+    private static class Road {
+        private int start;
+        private int end;
+
+        public Road(int[] road) {
+            this.start = road[0];
+            this.end = road[1];
+        }
+    }
+
+    /**
+     * Enhanced solution
+     */
+    public static int weeksToInfectAllCities_usingIterationByInfectedCities(int citiesAmount, int[][] roads, int[] infected) {
+        var routesMap = new HashMap<Integer, Set<Integer>>();
+        Arrays.stream(roads).forEach(road -> {
+            populate(routesMap, road, 0, 1);
+            populate(routesMap, road, 1, 0);
+        });
+
+        var weeksAmount = 0;
+        var infectedCities = Arrays.stream(infected).boxed().collect(Collectors.toSet());
+        var sourceCities = new HashSet<>(infectedCities);
+
+        while (!isAllInfected(citiesAmount, infectedCities)) {
+            if (sourceCities.isEmpty()) {
+                throw new IllegalArgumentException("Could not infect all cities!");
+            }
+
+            var newInfectedCities = determineNewInfectedCities(routesMap, infectedCities, sourceCities);
+            infectedCities.addAll(newInfectedCities);
+            sourceCities = newInfectedCities;
+            weeksAmount++;
+        }
+
+        return weeksAmount;
+    }
+
+    private static HashSet<Integer> determineNewInfectedCities(
+            Map<Integer, Set<Integer>> routesMap, Set<Integer> infectedCities, Set<Integer> sourceCities) {
+        var newInfectedCities = new HashSet<Integer>();
+        for (var city : sourceCities) {
+            if (!routesMap.containsKey(city)) {
+                continue;
+            }
+
+            var citiesToInfect = new HashSet<>(routesMap.get(city));
+            citiesToInfect.removeAll(infectedCities);
+
+            newInfectedCities.addAll(citiesToInfect);
+        }
+        return newInfectedCities;
+    }
+
+    private static void populate(Map<Integer, Set<Integer>> routesMap, int[] road, int i, int j) {
+        if (!routesMap.containsKey(road[i])) {
+            routesMap.put(road[i], new HashSet<>());
+        }
+        routesMap.get(road[i]).add(road[j]);
     }
 }
--- src/test/java/by/andd3dfx/common/InfectionDistributionTest.java
+++ src/test/java/by/andd3dfx/common/InfectionDistributionTest.java
@@ -2,27 +2,28 @@ package by.andd3dfx.common;
 
 import org.junit.Test;
 
-import static by.andd3dfx.common.InfectionDistribution.weeksToInfectAllCities;
+import static by.andd3dfx.common.InfectionDistribution.weeksToInfectAllCities_usingIterationByInfectedCities;
+import static by.andd3dfx.common.InfectionDistribution.weeksToInfectAllCities_usingIterationByRoads;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertThrows;
 
 public class InfectionDistributionTest {
 
     @Test
-    public void testWeeksToInfectAllCitiesPositive() {
-        assertThat(weeksToInfectAllCities(
+    public void weeksToInfectAllCitiesPositive_usingIterationByRoads() {
+        assertThat(weeksToInfectAllCities_usingIterationByRoads(
                 1,
                 new int[][]{},
                 new int[]{0}
         )).isEqualTo(0);
 
-        assertThat(weeksToInfectAllCities(
+        assertThat(weeksToInfectAllCities_usingIterationByRoads(
                 2,
                 new int[][]{new int[]{0, 1}},
                 new int[]{0}
         )).isEqualTo(1);
 
-        assertThat(weeksToInfectAllCities(
+        assertThat(weeksToInfectAllCities_usingIterationByRoads(
                 5,
                 new int[][]{
                         new int[]{0, 2},
@@ -32,7 +33,7 @@ public class InfectionDistributionTest {
                 new int[]{0, 3}
         )).isEqualTo(2);
 
-        assertThat(weeksToInfectAllCities(
+        assertThat(weeksToInfectAllCities_usingIterationByRoads(
                 7,
                 new int[][]{
                         new int[]{0, 2},
@@ -47,16 +48,74 @@ public class InfectionDistributionTest {
     }
 
     @Test
-    public void testWeeksToInfectAllCitiesNegative() {
+    public void weeksToInfectAllCitiesNegative_usingIterationByRoads() {
         assertThrows("Could not infect all cities!", IllegalArgumentException.class,
-                () -> weeksToInfectAllCities(
+                () -> weeksToInfectAllCities_usingIterationByRoads(
                         1,
                         new int[][]{},
                         new int[]{}
                 ));
 
         assertThrows("Could not infect all cities!", IllegalArgumentException.class,
-                () -> weeksToInfectAllCities(
+                () -> weeksToInfectAllCities_usingIterationByRoads(
+                        5,
+                        new int[][]{
+                                new int[]{0, 2},
+                                new int[]{1, 4},
+                        },
+                        new int[]{0, 3}
+                ));
+    }
+
+    @Test
+    public void weeksToInfectAllCitiesPositive_usingIterationByCities() {
+        assertThat(weeksToInfectAllCities_usingIterationByInfectedCities(
+                1,
+                new int[][]{},
+                new int[]{0}
+        )).isEqualTo(0);
+
+        assertThat(weeksToInfectAllCities_usingIterationByInfectedCities(
+                2,
+                new int[][]{new int[]{0, 1}},
+                new int[]{0}
+        )).isEqualTo(1);
+
+        assertThat(weeksToInfectAllCities_usingIterationByInfectedCities(
+                5,
+                new int[][]{
+                        new int[]{0, 2},
+                        new int[]{1, 2},
+                        new int[]{3, 4},
+                },
+                new int[]{0, 3}
+        )).isEqualTo(2);
+
+        assertThat(weeksToInfectAllCities_usingIterationByInfectedCities(
+                7,
+                new int[][]{
+                        new int[]{0, 2},
+                        new int[]{1, 2},
+                        new int[]{3, 4},
+                        new int[]{6, 4},
+                        new int[]{6, 5},
+                        new int[]{1, 5},
+                },
+                new int[]{0}
+        )).isEqualTo(6);
+    }
+
+    @Test
+    public void weeksToInfectAllCitiesNegative_usingIterationByCities() {
+        assertThrows("Could not infect all cities!", IllegalArgumentException.class,
+                () -> weeksToInfectAllCities_usingIterationByInfectedCities(
+                        1,
+                        new int[][]{},
+                        new int[]{}
+                ));
+
+        assertThrows("Could not infect all cities!", IllegalArgumentException.class,
+                () -> weeksToInfectAllCities_usingIterationByInfectedCities(
                         5,
                         new int[][]{
                                 new int[]{0, 2},
