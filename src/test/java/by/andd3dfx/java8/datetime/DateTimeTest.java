f1588acb708cdcdc668fcdd5f3b91562d940a39e
Andrei Punko <andd3dfx@gmail.com>
2023-07-13 11:58:04 +0300

    Reduce amount of spam into System.out during tests

--- src/test/java/by/andd3dfx/java8/datetime/DateTimeTest.java
+++ src/test/java/by/andd3dfx/java8/datetime/DateTimeTest.java
@@ -30,13 +30,12 @@ public class DateTimeTest {
 
     @Test
     public void zoneId() {
-        System.out.println(ZoneId.getAvailableZoneIds());
-        // prints all available timezone ids
+        ZoneId.getAvailableZoneIds();   // all available timezone ids
 
         ZoneId zone1 = ZoneId.of("Europe/Berlin");
         ZoneId zone2 = ZoneId.of("Brazil/East");
-        System.out.println(zone1.getRules());       // ZoneRules[currentStandardOffset=+01:00]
-        System.out.println(zone2.getRules());       // ZoneRules[currentStandardOffset=-03:00]
+        var zoneRules1 = zone1.getRules();       // ZoneRules[currentStandardOffset=+01:00]
+        var zoneRules2 = zone2.getRules();       // ZoneRules[currentStandardOffset=-03:00]
     }
 
     @Test
@@ -47,24 +46,18 @@ public class DateTimeTest {
         LocalTime now1 = LocalTime.now(zone1);
         LocalTime now2 = LocalTime.now(zone2);
 
-        System.out.println(now1.isBefore(now2));  // false
+        var isNow1BeforeNow2 = now1.isBefore(now2);  // false
 
-        long hoursBetween = ChronoUnit.HOURS.between(now1, now2);
-        long minutesBetween = ChronoUnit.MINUTES.between(now1, now2);
+        long hoursBetween = ChronoUnit.HOURS.between(now1, now2);       // -3
+        long minutesBetween = ChronoUnit.MINUTES.between(now1, now2);   // -239
 
-        System.out.println(hoursBetween);       // -3
-        System.out.println(minutesBetween);     // -239
+        LocalTime late = LocalTime.of(23, 59, 59);   // 23:59:59
 
-        LocalTime late = LocalTime.of(23, 59, 59);
-        System.out.println(late);       // 23:59:59
+        DateTimeFormatter germanFormatter = DateTimeFormatter
+                .ofLocalizedTime(FormatStyle.SHORT)
+                .withLocale(Locale.GERMAN);
 
-        DateTimeFormatter germanFormatter =
-                DateTimeFormatter
-                        .ofLocalizedTime(FormatStyle.SHORT)
-                        .withLocale(Locale.GERMAN);
-
-        LocalTime leetTime = LocalTime.parse("13:37", germanFormatter);
-        System.out.println(leetTime);   // 13:37
+        LocalTime germanTime = LocalTime.parse("13:37", germanFormatter); // 13:37
     }
 
     @Test
@@ -74,8 +67,7 @@ public class DateTimeTest {
         LocalDate yesterday = tomorrow.minusDays(2);
 
         LocalDate independenceDay = LocalDate.of(2014, Month.JULY, 4);
-        DayOfWeek dayOfWeek = independenceDay.getDayOfWeek();
-        System.out.println(dayOfWeek);    // FRIDAY
+        DayOfWeek dayOfWeek = independenceDay.getDayOfWeek();       // FRIDAY
 
         // Create LocalDate by parsing string:
         DateTimeFormatter germanFormatter =
@@ -83,37 +75,31 @@ public class DateTimeTest {
                         .ofLocalizedDate(FormatStyle.MEDIUM)
                         .withLocale(Locale.GERMAN);
 
-        LocalDate xmas = LocalDate.parse("24.12.2014", germanFormatter);
-        System.out.println(xmas);   // 2014-12-24
+        LocalDate xmas = LocalDate.parse("24.12.2014", germanFormatter);    // 2014-12-24
     }
 
     @Test
     public void localDateTime() {
         LocalDateTime sylvester = LocalDateTime.of(2014, Month.DECEMBER, 31, 23, 59, 59);
 
-        DayOfWeek dayOfWeek = sylvester.getDayOfWeek();
-        System.out.println(dayOfWeek);      // WEDNESDAY
+        DayOfWeek dayOfWeek = sylvester.getDayOfWeek(); // WEDNESDAY
 
-        Month month = sylvester.getMonth();
-        System.out.println(month);          // DECEMBER
+        Month month = sylvester.getMonth();             // DECEMBER
 
-        long minuteOfDay = sylvester.getLong(ChronoField.MINUTE_OF_DAY);
-        System.out.println(minuteOfDay);    // 1439
+        long minuteOfDay = sylvester.getLong(ChronoField.MINUTE_OF_DAY);    // 1439
 
         // Get an instant:
         Instant instant = sylvester
                 .atZone(ZoneId.systemDefault())
                 .toInstant();
 
-        Date legacyDate = Date.from(instant);
-        System.out.println(legacyDate);     // Wed Dec 31 23:59:59 CET 2014
+        Date legacyDate = Date.from(instant);       // Wed Dec 31 23:59:59 CET 2014
 
         // Formatting:
         DateTimeFormatter formatter = DateTimeFormatter
                 .ofPattern("MMM dd, yyyy - HH:mm", Locale.US);
 
-        LocalDateTime parsed = LocalDateTime.parse("Nov 03, 2014 - 07:13", formatter);
-        String string = formatter.format(parsed);
-        System.out.println(string);     // Nov 03, 2014 - 07:13
+        LocalDateTime parsedLocalDateTime = LocalDateTime.parse("Nov 03, 2014 - 07:13", formatter);
+        String localDateTimeString = formatter.format(parsedLocalDateTime);   // Nov 03, 2014 - 07:13
     }
 }
