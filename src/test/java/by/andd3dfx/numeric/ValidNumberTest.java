b77a47aaf712bb467f72f84339f9058f8a3dc688
Andrei Punko <andd3dfx@gmail.com>
2023-07-24 13:41:17 +0300

    Make regex for ValidNumber a bit more compact

--- src/test/java/by/andd3dfx/numeric/ValidNumberTest.java
+++ src/test/java/by/andd3dfx/numeric/ValidNumberTest.java
@@ -3,16 +3,11 @@ package by.andd3dfx.numeric;
 import org.junit.Test;
 
 import static by.andd3dfx.numeric.ValidNumber.isNumber;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class ValidNumberTest {
 
-    /**
-     * For example, all the following are valid numbers:
-     * ["2", "0089", "-0.1", "+3.14", "4.", "-.9", "2e10", "-90E3", "3e+7", "+6e-1", "53.5e93", "-123.456e789"],
-     * while the following are not valid numbers:
-     * ["abc", "1a", "1e", "e3", "99e2.5", "--6", "-+3", "95a54e53"].
-     */
     @Test
     public void testIsNumber() {
         assertTrue(isNumber("2"));
