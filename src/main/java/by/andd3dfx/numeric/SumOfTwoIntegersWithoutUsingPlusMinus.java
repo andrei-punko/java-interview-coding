7aa1df31554a26bf99d15d017727f58ed80267a4
Andrei Punko <andd3dfx@gmail.com>
2023-09-06 09:28:57 +0300

    Add optimized version of SumOfTwoIntegersWithoutUsingPlusMinus

--- src/main/java/by/andd3dfx/numeric/SumOfTwoIntegersWithoutUsingPlusMinus.java
+++ src/main/java/by/andd3dfx/numeric/SumOfTwoIntegersWithoutUsingPlusMinus.java
@@ -19,6 +19,9 @@ package by.andd3dfx.numeric;
  */
 public class SumOfTwoIntegersWithoutUsingPlusMinus {
 
+    /**
+     * Doesn't support the addition of negative numbers
+     */
     public static int getSum(int a, int b) {
         int res = a;
 
@@ -40,5 +43,35 @@ public class SumOfTwoIntegersWithoutUsingPlusMinus {
         number ^= shifted_1;
         var n_plus_1 = incNthBit(n, 0);     // To avoid using `+` in `n+1` expression :)
         return incNthBit(number, n_plus_1);
+    }
+
+    /**
+     * 3 + 5    a + b
+     * --------------------
+     * 0b0011   a
+     * 0b0101   b
+     * <p>
+     * 0b0110   a^b
+     * 0b0010   (a & b) << 1
+     * --------------------
+     * 0b0110   a2
+     * 0b0010   b2
+     * <p>
+     * 0b0100   a2^b2
+     * 0b0100   (a2 & b2) << 1
+     * --------------------
+     * 0b0100   a3
+     * 0b0100   b3
+     * <p>
+     * 0b0000   a3^b3
+     * 0b1000   (a3 & b3) << 1
+     * --------------------
+     * 0b0000   a4 = 0 => b4 - result
+     * 0b1000   b4 = 8
+     */
+    public static int getSumOptimized(int a, int b) {
+        if (a == 0) return b;
+        if (b == 0) return a;
+        return getSumOptimized(a ^ b, (a & b) << 1);
     }
 }
--- src/test/java/by/andd3dfx/numeric/SumOfTwoIntegersWithoutUsingPlusMinusTest.java
+++ src/test/java/by/andd3dfx/numeric/SumOfTwoIntegersWithoutUsingPlusMinusTest.java
@@ -1,9 +1,9 @@
 package by.andd3dfx.numeric;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static by.andd3dfx.numeric.SumOfTwoIntegersWithoutUsingPlusMinus.getSum;
+import static by.andd3dfx.numeric.SumOfTwoIntegersWithoutUsingPlusMinus.getSumOptimized;
 import static by.andd3dfx.numeric.SumOfTwoIntegersWithoutUsingPlusMinus.incNthBit;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -35,20 +35,28 @@ public class SumOfTwoIntegersWithoutUsingPlusMinusTest {
         assertThat(getSum(234, 67)).isEqualTo(301);
     }
 
-    @Ignore // TODO: add support of addition of negative numbers
-    @Test
-    public void testGetSumForNegative() {
-        assertThat(getSum(-1, 1)).isEqualTo(0);
-        assertThat(getSum(-5, 7)).isEqualTo(2);
-        assertThat(getSum(-4, 2)).isEqualTo(-2);
-        assertThat(getSum(-4, -5)).isEqualTo(-9);
-    }
-
     @Test
     public void testIncNthBit() {
         assertThat(incNthBit(1, 0)).isEqualTo(2);
         assertThat(incNthBit(3, 1)).isEqualTo(5);
         assertThat(incNthBit(15, 2)).isEqualTo(19);
         assertThat(incNthBit(15, 4)).isEqualTo(31);
+    }
+
+    @Test
+    public void testGetSumOptimized() {
+        assertThat(getSumOptimized(1, 0)).isEqualTo(1);
+        assertThat(getSumOptimized(1, 2)).isEqualTo(3);
+        assertThat(getSumOptimized(4, 3)).isEqualTo(7);
+        assertThat(getSumOptimized(5, 11)).isEqualTo(16);
+        assertThat(getSumOptimized(234, 67)).isEqualTo(301);
+    }
+
+    @Test
+    public void testGetSumsForNegative() {
+        assertThat(getSumOptimized(-1, 1)).isEqualTo(0);
+        assertThat(getSumOptimized(-5, 7)).isEqualTo(2);
+        assertThat(getSumOptimized(-4, 2)).isEqualTo(-2);
+        assertThat(getSumOptimized(-4, -5)).isEqualTo(-9);
     }
 }
