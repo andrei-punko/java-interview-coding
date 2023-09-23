9464d8a4f79e985c83477a76458cfe3a2e0995b1
Andrei Punko <andd3dfx@gmail.com>
2023-09-23 12:51:13 +0300

    Add CheckCapitalUsage task solution

---
+++ src/main/java/by/andd3dfx/string/CheckCapitalUsage.java
@@ -0,0 +1,51 @@
+package by.andd3dfx.string;
+
+import java.util.Set;
+import java.util.regex.Pattern;
+
+/**
+ * <pre>
+ * https://leetcode.com/problems/detect-capital/description/
+ *
+ * We define the usage of capitals in a word to be right when one of the following cases holds:
+ *
+ *     All letters in this word are capitals, like "USA".
+ *     All letters in this word are not capitals, like "leetcode".
+ *     Only the first letter in this word is capital, like "Google".
+ *
+ * Given a string word, return true if the usage of capitals in it is right.
+ *
+ * Example 1:
+ *
+ * Input: word = "USA"
+ * Output: true
+ *
+ * Example 2:
+ *
+ * Input: word = "FlaG"
+ * Output: false
+ * </pre>
+ */
+public class CheckCapitalUsage {
+
+    public boolean isCapitalUsedProperly(String word) {
+        var uppercase = word.toUpperCase();
+        var lowercase = word.toLowerCase();
+        var onlyFirstLetterCapitalized = buildWordWithFirstLetterCapitalized(word);
+
+        return Set.of(uppercase, lowercase, onlyFirstLetterCapitalized)
+                .contains(word);
+    }
+
+    private String buildWordWithFirstLetterCapitalized(String word) {
+        var chars = word.toLowerCase().toCharArray();
+        chars[0] = Character.toUpperCase(chars[0]);
+        return new String(chars);
+    }
+
+    private final static Pattern PATTERN = Pattern.compile("^([A-Z]*|[A-Z]?[a-z]*)$");
+
+    public boolean isCapitalUsedProperly_withRegex(String word) {
+        return PATTERN.matcher(word).find();
+    }
+}
---
+++ src/test/java/by/andd3dfx/string/CheckCapitalUsageTest.java
@@ -0,0 +1,35 @@
+package by.andd3dfx.string;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class CheckCapitalUsageTest {
+
+    private CheckCapitalUsage checkCapitalUsage;
+
+    @Before
+    public void setUp() throws Exception {
+        checkCapitalUsage = new CheckCapitalUsage();
+    }
+
+    @Test
+    public void isCapitalUsedProperly() {
+        assertTrue(checkCapitalUsage.isCapitalUsedProperly("USA"));
+        assertTrue(checkCapitalUsage.isCapitalUsedProperly("Google"));
+        assertTrue(checkCapitalUsage.isCapitalUsedProperly("leetcode"));
+        assertFalse(checkCapitalUsage.isCapitalUsedProperly("FlaG"));
+        assertFalse(checkCapitalUsage.isCapitalUsedProperly("flAg"));
+    }
+
+    @Test
+    public void isCapitalUsedProperly_withRegex() {
+        assertTrue(checkCapitalUsage.isCapitalUsedProperly_withRegex("USA"));
+        assertTrue(checkCapitalUsage.isCapitalUsedProperly_withRegex("Google"));
+        assertTrue(checkCapitalUsage.isCapitalUsedProperly_withRegex("leetcode"));
+        assertFalse(checkCapitalUsage.isCapitalUsedProperly_withRegex("FlaG"));
+        assertFalse(checkCapitalUsage.isCapitalUsedProperly_withRegex("flAg"));
+    }
+}
