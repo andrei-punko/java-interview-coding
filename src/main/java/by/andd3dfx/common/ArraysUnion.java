932f2af1440bb045b6ea71cad060c1b549c18565
Andrei Punko <andd3dfx@gmail.com>
2023-11-26 18:33:50 +0300

    Adjust ArraysUnion task solutions

--- src/main/java/by/andd3dfx/common/ArraysUnion.java
+++ src/main/java/by/andd3dfx/common/ArraysUnion.java
@@ -2,25 +2,16 @@ package by.andd3dfx.common;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
- * Даны 2 упорядоченных (отсортированных) массива с уникальными элементами,
+ * Даны 2 упорядоченных массива с уникальными элементами,
  * найти и вывести их упорядоченное объединение без дубликатов.
  */
 public class ArraysUnion {
 
-    public static Integer[] uniteUsingStreams(int[] a, int[] b) {
-        return Stream.concat(Arrays.stream(a).boxed(), Arrays.stream(b).boxed())
-                .collect(Collectors.toSet())
-                .stream().sorted()
-                .collect(Collectors.toList())
-                .toArray(new Integer[0]);
-    }
-
-    public static Integer[] uniteUsingManualIteration(int[] a, int[] b) {
+    public static int[] unite(int[] a, int[] b) {
         List<Integer> result = new ArrayList<>();
 
         int aIndex = 0;
@@ -31,17 +22,11 @@ public class ArraysUnion {
         while (aIndex < a.length && bIndex < b.length) {
             if (a[aIndex] <= b[bIndex]) {
                 var itemToAdd = a[aIndex];
-                if (lastAdded == null || itemToAdd != lastAdded) {
-                    result.add(itemToAdd);
-                    lastAdded = itemToAdd;
-                }
+                lastAdded = addItemToArray(result, itemToAdd, lastAdded);
                 aIndex++;
             } else {
                 var itemToAdd = b[bIndex];
-                if (lastAdded == null || itemToAdd != lastAdded) {
-                    result.add(itemToAdd);
-                    lastAdded = itemToAdd;
-                }
+                lastAdded = addItemToArray(result, itemToAdd, lastAdded);
                 bIndex++;
             }
         }
@@ -52,6 +37,44 @@ public class ArraysUnion {
         Arrays.stream(Arrays.copyOfRange(b, bIndex, b.length))
                 .forEach(result::add);
 
-        return result.toArray(new Integer[0]);
+        return result.stream()
+                .mapToInt(value -> value)
+                .toArray();
+    }
+
+    private static Integer addItemToArray(List<Integer> array, int itemToAdd, Integer lastAdded) {
+        if (lastAdded == null || itemToAdd != lastAdded) {
+            array.add(itemToAdd);
+            lastAdded = itemToAdd;
+        }
+        return lastAdded;
+    }
+
+    public static int[] uniteUsingSet(int[] a, int[] b) {
+        LinkedHashSet<Integer> result = new LinkedHashSet<>();
+
+        int aIndex = 0;
+        int bIndex = 0;
+
+        // Process overlapping part of arrays
+        while (aIndex < a.length && bIndex < b.length) {
+            if (a[aIndex] <= b[bIndex]) {
+                result.add(a[aIndex]);
+                aIndex++;
+            } else {
+                result.add(b[bIndex]);
+                bIndex++;
+            }
+        }
+
+        // Add the remaining part of one of the arrays
+        Arrays.stream(Arrays.copyOfRange(a, aIndex, a.length))
+                .forEach(result::add);
+        Arrays.stream(Arrays.copyOfRange(b, bIndex, b.length))
+                .forEach(result::add);
+
+        return result.stream()
+                .mapToInt(value -> value)
+                .toArray();
     }
 }
--- src/test/java/by/andd3dfx/common/ArraysUnionTest.java
+++ src/test/java/by/andd3dfx/common/ArraysUnionTest.java
@@ -2,37 +2,37 @@ package by.andd3dfx.common;
 
 import org.junit.Test;
 
+import static by.andd3dfx.common.ArraysUnion.unite;
+import static by.andd3dfx.common.ArraysUnion.uniteUsingSet;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ArraysUnionTest {
 
     @Test
-    public void uniteUsingStreams() {
-        // Overlapping arrays case
-        assertThat(ArraysUnion.uniteUsingStreams(new int[]{1, 2, 3, 7}, new int[]{2, 3, 4, 6}))
-                .isEqualTo(new Integer[]{1, 2, 3, 4, 6, 7});
-        assertThat(ArraysUnion.uniteUsingStreams(new int[]{2, 3, 4, 6}, new int[]{1, 2, 3, 7}))
-                .isEqualTo(new Integer[]{1, 2, 3, 4, 6, 7});
-
-        // Non-overlapping arrays case
-        assertThat(ArraysUnion.uniteUsingStreams(new int[]{1, 2, 3}, new int[]{10, 11, 12}))
-                .isEqualTo(new Integer[]{1, 2, 3, 10, 11, 12});
-        assertThat(ArraysUnion.uniteUsingStreams(new int[]{10, 11, 12}, new int[]{1, 2, 3}))
-                .isEqualTo(new Integer[]{1, 2, 3, 10, 11, 12});
+    public void unite_forOverlappingArrays() {
+        assertThat(unite(new int[]{1, 2, 2, 5, 7, 9}, new int[]{2, 3, 4, 4, 6}))
+                .isEqualTo(new int[]{1, 2, 3, 4, 5, 6, 7, 9});
     }
 
     @Test
-    public void uniteUsingManualIteration() {
-        // Overlapping arrays case
-        assertThat(ArraysUnion.uniteUsingManualIteration(new int[]{1, 2, 3, 7}, new int[]{2, 3, 4, 6}))
-                .isEqualTo(new Integer[]{1, 2, 3, 4, 6, 7});
-        assertThat(ArraysUnion.uniteUsingManualIteration(new int[]{2, 3, 4, 6}, new int[]{1, 2, 3, 7}))
-                .isEqualTo(new Integer[]{1, 2, 3, 4, 6, 7});
+    public void unite_forNonOverlappingArrays() {
+        assertThat(unite(new int[]{1, 2, 3}, new int[]{10, 11, 12}))
+                .isEqualTo(new int[]{1, 2, 3, 10, 11, 12});
+        assertThat(unite(new int[]{10, 11, 12}, new int[]{1, 2, 3}))
+                .isEqualTo(new int[]{1, 2, 3, 10, 11, 12});
+    }
 
-        // Non-overlapping arrays case
-        assertThat(ArraysUnion.uniteUsingManualIteration(new int[]{1, 2, 3}, new int[]{10, 11, 12}))
-                .isEqualTo(new Integer[]{1, 2, 3, 10, 11, 12});
-        assertThat(ArraysUnion.uniteUsingManualIteration(new int[]{10, 11, 12}, new int[]{1, 2, 3}))
-                .isEqualTo(new Integer[]{1, 2, 3, 10, 11, 12});
+    @Test
+    public void uniteUsingSet_forOverlappingArrays() {
+        assertThat(uniteUsingSet(new int[]{1, 2, 2, 5, 7, 9}, new int[]{2, 3, 4, 4, 6}))
+                .isEqualTo(new int[]{1, 2, 3, 4, 5, 6, 7, 9});
+    }
+
+    @Test
+    public void uniteUsingSet_forNonOverlappingArrays() {
+        assertThat(uniteUsingSet(new int[]{1, 2, 3}, new int[]{10, 11, 12}))
+                .isEqualTo(new int[]{1, 2, 3, 10, 11, 12});
+        assertThat(uniteUsingSet(new int[]{10, 11, 12}, new int[]{1, 2, 3}))
+                .isEqualTo(new int[]{1, 2, 3, 10, 11, 12});
     }
 }
