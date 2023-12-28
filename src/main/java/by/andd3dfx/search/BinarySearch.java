0ef7f786e416c306eac59eeced8840458bf413cc
Andrei Punko <andd3dfx@gmail.com>
2023-12-28 15:37:21 +0300

    Fix BinarySearch implementation (was some error)

--- src/main/java/by/andd3dfx/search/BinarySearch.java
+++ src/main/java/by/andd3dfx/search/BinarySearch.java
@@ -1,34 +1,32 @@
 package by.andd3dfx.search;
 
 /**
- * Implement binary search in sorted array. Return element index or -1 if it doesn't exist
+ * Implement binary search in a sorted array. Return element index or -1 if it doesn't exist
  */
 public class BinarySearch {
 
-  public static int perform(int[] array, int target) {
-    int left = 0;
-    int right = array.length;
-    int steps = 0;
+    public static int perform(int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+        int steps = 0;
 
-    try {
-      do {
-        steps++;
+        try {
+            while (left <= right) {
+                steps++;
 
-        int middle = (left + right) / 2;
-        if (array[middle] == target) {
-          return middle;
+                int middle = Math.floorDiv(left + right, 2);
+                if (array[middle] < target) {
+                    left = middle + 1;
+                } else if (array[middle] > target) {
+                    right = middle - 1;
+                } else {
+                    return middle;
+                }
+            }
+
+            return -1;
+        } finally {
+            System.out.println("Total steps count: " + steps);
         }
-
-        if (array[middle] < target) {
-          left = middle;
-        } else {
-          right = middle;
-        }
-      } while (right - left > 1);
-
-      return -1;
-    } finally {
-      System.out.println("target: " + target + ", total steps count: " + steps);
     }
-  }
 }
--- src/test/java/by/andd3dfx/search/BinarySearchTest.java
+++ src/test/java/by/andd3dfx/search/BinarySearchTest.java
@@ -2,20 +2,28 @@ package by.andd3dfx.search;
 
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static by.andd3dfx.search.BinarySearch.perform;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class BinarySearchTest {
 
     @Test
-    public void perform() {
-        assertThat(BinarySearch.perform(new int[]{1, 2, 3, 4, 5}, 4), is(3));
-        assertThat(BinarySearch.perform(new int[]{1, 2, 2, 3, 4, 8}, 2), is(1));
-        assertThat(BinarySearch.perform(new int[]{-2, -1, 1, 2, 5, 8, 9, 12, 15}, 12), is(7));
+    public void testPerform() {
+        assertThat(perform(new int[]{1, 3}, 1)).isEqualTo(0);
+        assertThat(perform(new int[]{1, 3}, 3)).isEqualTo(1);
+        assertThat(perform(new int[]{1, 2, 3, 4, 5}, 4)).isEqualTo(3);
+        assertThat(perform(new int[]{1, 2, 2, 3, 4, 8}, 2)).isEqualTo(2);
+        assertThat(perform(new int[]{-2, -1, 1, 2, 5, 8, 9, 12, 15}, 12)).isEqualTo(7);
     }
 
     @Test
-    public void performWhenNothingFound() {
-        assertThat(BinarySearch.perform(new int[]{1, 2, 3, 4, 5}, 41), is(-1));
+    public void testPerformWhenNothingFound() {
+        assertThat(perform(new int[]{1, 2, 3, 4, 5}, 41)).isEqualTo(-1);
+        assertThat(perform(new int[]{1, 2, 30, 40, 50, 51}, 35)).isEqualTo(-1);
+    }
+
+    @Test
+    public void testPerformForEmptyArray() {
+        assertThat(perform(new int[]{}, 0)).isEqualTo(-1);
     }
 }
