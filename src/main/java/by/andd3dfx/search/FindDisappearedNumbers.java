71bf9381806bf58c4ce9698908e9fef8da6b8aa9
Andrei Punko <andd3dfx@gmail.com>
2024-01-26 15:29:01 +0300

    Add JavaDoc and some logging into FindDisappearedNumbers solution

--- src/main/java/by/andd3dfx/search/FindDisappearedNumbers.java
+++ src/main/java/by/andd3dfx/search/FindDisappearedNumbers.java
@@ -3,12 +3,37 @@ package by.andd3dfx.search;
 import java.util.Arrays;
 
 /**
+ * <pre>
  * Дан неупорядоченный массив из N чисел от 1 до N,
  * при этом несколько чисел из диапазона [1, N] пропущено, а некоторые присутствуют дважды.
  * Найти все пропущенные числа без использования дополнительной памяти.
+ * </pre>
  */
 public class FindDisappearedNumbers {
 
+    /**
+     * <pre>
+     * Use item sign to store an info is this number happens before or not:
+     *  input: [1 3 3]
+     *  [-1 3 3]
+     *  [-1 3 -3]
+     *  [-1 3 -3]
+     * ---
+     * output: [2]
+     *
+     * input: [3 3 3]
+     * ...
+     * [3 3 -3]
+     * ---
+     * output: [1 2]
+     *
+     * input: [3 1 2]
+     * ...
+     * [-3 -1 -2]
+     * ---
+     * output: []
+     * </pre>
+     */
     public static int[] find(int[] items) {
         for (int i = 0; i < items.length; i++) {
             var index = Math.abs(items[i]) - 1;
@@ -16,6 +41,7 @@ public class FindDisappearedNumbers {
                 items[index] = -items[index];
             }
         }
+        System.out.println(Arrays.toString(items));
 
         var amount = 0;
         for (int i = 0; i < items.length; i++) {
