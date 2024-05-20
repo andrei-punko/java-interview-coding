32e774ec27dbaefbc4b24d0193dbdce7f2370da6
Andrei Punko <andd3dfx@gmail.com>
2024-05-20 16:21:48 +0300

    Add more solutions for EratosthenesSieve & rename class

---
+++ src/main/java/by/andd3dfx/numeric/AmountOfPrimeNumbers.java
@@ -0,0 +1,68 @@
+package by.andd3dfx.numeric;
+
+import java.util.Arrays;
+import java.util.BitSet;
+
+/**
+ * Find amount of prime numbers which less than definite number N.
+ * <p>
+ * See <a href="https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes">Sieve of Eratosthenes</a>
+ */
+public class AmountOfPrimeNumbers {
+
+    public static int determine_usingPrimeDividersOfNumberSolution(int n) {
+        int result = 0;
+        for (int i = 1; i < n; i++) {
+            if (isPrime(i)) {
+                result++;
+            }
+        }
+        return result;
+    }
+
+    private static boolean isPrime(int number) {
+        return PrimeDividersOfNumber.determine(number).length == 1;
+    }
+
+    public static int determine_usingEratosthenesSieve(int n) {
+        var isPrime = new boolean[n];
+        Arrays.fill(isPrime, 2, n, true);
+
+        for (var i = 2; i < n; i++) {
+            if (isPrime[i]) {
+                for (var j = 2; j * i < n; j++) {
+                    isPrime[i * j] = false;
+                }
+            }
+        }
+
+        int result = 0;
+        for (var item : isPrime) {
+            if (item) {
+                result++;
+            }
+        }
+        return result;
+    }
+
+    public static int determine_usingEratosthenesSieveWithBitSet(int n) {
+        var isPrime = new BitSet(n);
+        isPrime.set(2, n);
+
+        for (var i = 2; i < n; i++) {
+            if (isPrime.get(i)) {
+                for (var j = 2; j * i < n; j++) {
+                    isPrime.clear(i * j);
+                }
+            }
+        }
+
+        int result = 0;
+        for (var i = 0; i < n; i++) {
+            if (isPrime.get(i)) {
+                result++;
+            }
+        }
+        return result;
+    }
+}
---
+++ src/test/java/by/andd3dfx/numeric/AmountOfPrimeNumbersTest.java
@@ -0,0 +1,39 @@
+package by.andd3dfx.numeric;
+
+import org.junit.Test;
+
+import java.util.function.Function;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AmountOfPrimeNumbersTest {
+
+    @Test
+    public void testDetermine_usingPrimeDividersOfNumberSolution() {
+        check(AmountOfPrimeNumbers::determine_usingPrimeDividersOfNumberSolution);
+    }
+
+    @Test
+    public void testDetermine_usingEratosthenesSieve() {
+        check(AmountOfPrimeNumbers::determine_usingEratosthenesSieve);
+    }
+
+    @Test
+    public void testDetermine_usingEratosthenesSieveWithBitSet() {
+        check(AmountOfPrimeNumbers::determine_usingEratosthenesSieveWithBitSet);
+    }
+
+    private void check(Function<Integer, Integer> function) {
+        assertThat(function.apply(2)).isEqualTo(0);
+        assertThat(function.apply(3)).isEqualTo(1);
+        assertThat(function.apply(4)).isEqualTo(2);
+        assertThat(function.apply(5)).isEqualTo(2);
+        assertThat(function.apply(6)).isEqualTo(3);
+        assertThat(function.apply(7)).isEqualTo(3);
+        assertThat(function.apply(8)).isEqualTo(4);
+        assertThat(function.apply(10)).isEqualTo(4);
+        assertThat(function.apply(11)).isEqualTo(4);
+        assertThat(function.apply(12)).isEqualTo(5);
+        assertThat(function.apply(15)).isEqualTo(6);
+    }
+}
