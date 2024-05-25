0c0e065f4fcd566ac7dbf51a95c759399731f0a0
Andrei Punko <andd3dfx@gmail.com>
2024-05-25 19:52:31 +0300

    Make HowToStartMultipleTasks a bit more compact

--- src/main/java/by/andd3dfx/multithreading/HowToStartMultipleTasks.java
+++ src/main/java/by/andd3dfx/multithreading/HowToStartMultipleTasks.java
@@ -2,6 +2,7 @@ package by.andd3dfx.multithreading;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinTask;
@@ -15,28 +16,22 @@ import java.util.concurrent.RecursiveTask;
 public class HowToStartMultipleTasks {
 
     public void usingThreads() {
-        var thread1 = new Thread(() -> { log.info("--- usingThreads: Make action 1"); });
-        var thread2 = new Thread(() -> { log.info("--- usingThreads: Make action 2"); });
-        var thread3 = new Thread(() -> { log.info("--- usingThreads: Make action 3"); });
-        var thread4 = new Thread(() -> { log.info("--- usingThreads: Make action 4"); });
-        var thread5 = new Thread(() -> { log.info("--- usingThreads: Make action 5"); });
+        var thread1 = new Thread(() -> log.info("--- usingThreads: Make action 1"));
+        var thread2 = new Thread(() -> log.info("--- usingThreads: Make action 2"));
+        var thread3 = new Thread(() -> log.info("--- usingThreads: Make action 3"));
 
         thread1.start();
         thread2.start();
         thread3.start();
-        thread4.start();
-        thread5.start();
     }
 
-    public void usingCompletableFuture() throws ExecutionException, InterruptedException {
+    public void usingCompletableFuture() {
         CompletableFuture<Void> combinedFuture = CompletableFuture.allOf(
-                CompletableFuture.runAsync(() -> { log.info("    usingCompletableFuture: Make action 1"); }),
-                CompletableFuture.runAsync(() -> { log.info("    usingCompletableFuture: Make action 2"); }),
-                CompletableFuture.runAsync(() -> { log.info("    usingCompletableFuture: Make action 3"); }),
-                CompletableFuture.runAsync(() -> { log.info("    usingCompletableFuture: Make action 4"); }),
-                CompletableFuture.runAsync(() -> { log.info("    usingCompletableFuture: Make action 5"); })
+                CompletableFuture.runAsync(() -> log.info("    usingCompletableFuture: Make action 1")),
+                CompletableFuture.runAsync(() -> log.info("    usingCompletableFuture: Make action 2")),
+                CompletableFuture.runAsync(() -> log.info("    usingCompletableFuture: Make action 3"))
         );
-        combinedFuture.get();
+        combinedFuture.join();
     }
 
     public void usingForkJoinTask() {
@@ -62,28 +57,12 @@ public class HowToStartMultipleTasks {
                         log.info("usingForkJoinTask: Make action 3");
                         return null;
                     }
-                },
-                new RecursiveTask() {
-                    @Override
-                    protected Object compute() {
-                        // Add result 4 computation
-                        log.info("usingForkJoinTask: Make action 4");
-                        return null;
-                    }
-                },
-                new RecursiveTask() {
-                    @Override
-                    protected Object compute() {
-                        // Add result 5 computation
-                        log.info("usingForkJoinTask: Make action 5");
-                        return null;
-                    }
                 }
         );
     }
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        HowToStartMultipleTasks starter = new HowToStartMultipleTasks();
+        var starter = new HowToStartMultipleTasks();
         starter.usingThreads();
         starter.usingCompletableFuture();
         starter.usingForkJoinTask();
