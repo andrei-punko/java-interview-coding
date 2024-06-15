0f1cf59835451efa41b6f0efbffd68bf917e49fa
Andrei Punko <andd3dfx@gmail.com>
2024-06-15 23:30:56 +0300

    Remove primitive examples of usage Thread/Runnable

--- src/main/java/by/andd3dfx/multithreading/HelloRunnable.java
+++
@@ -1,8 +0,0 @@
-package by.andd3dfx.multithreading;
-
-public class HelloRunnable implements Runnable {
-    @Override
-    public void run() {
-        System.out.printf("Hello, I'm runnable");
-    }
-}
--- src/main/java/by/andd3dfx/multithreading/HelloThread.java
+++
@@ -1,8 +0,0 @@
-package by.andd3dfx.multithreading;
-
-public class HelloThread extends Thread {
-    @Override
-    public void run() {
-        System.out.printf("Hello, I'm thread");
-    }
-}
