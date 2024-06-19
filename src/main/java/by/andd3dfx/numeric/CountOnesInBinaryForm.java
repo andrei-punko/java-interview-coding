474eefcbd3e6cc2ba1e254d8b7c1df36c4db6d0e
Andrei Punko <andd3dfx@gmail.com>
2024-06-19 17:31:17 +0300

    CountOnesInBinaryForm: add reference to leetcode in javadoc, update task name in README

--- src/main/java/by/andd3dfx/numeric/CountOnesInBinaryForm.java
+++ src/main/java/by/andd3dfx/numeric/CountOnesInBinaryForm.java
@@ -1,7 +1,30 @@
 package by.andd3dfx.numeric;
 
 /**
- * Count ones in binary form of number
+ * <pre>
+ * https://leetcode.com/problems/number-of-1-bits/description/
+ *
+ * Write a function that takes the binary representation of a positive integer and returns
+ * the number of set bits it has (also known as the Hamming weight).
+ *
+ * Example 1:
+ *   Input: n = 11
+ *   Output: 3
+ *   Explanation:
+ *   The input binary string 1011 has a total of three set bits.
+ *
+ * Example 2:
+ *   Input: n = 128
+ *   Output: 1
+ *   Explanation:
+ *   The input binary string 10000000 has a total of one set bit.
+ *
+ * Example 3:
+ *   Input: n = 2147483645
+ *   Output: 30
+ *   Explanation:
+ *   The input binary string 1111111111111111111111111111101 has a total of thirty set bits.
+ * </pre>
  *
  * @see <a href="https://youtu.be/F8zwvJYw0R8">Video solution</a>
  */
