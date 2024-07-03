226521b1246e3e29d0416307fe719d3073f0ddd2
Andrei Punko <andd3dfx@gmail.com>
2024-07-03 19:29:46 +0300

    ContainsDuplicates: rewrite tests for readability and avoid duplication

--- src/test/java/by/andd3dfx/collections/ContainsDuplicatesTest.java
+++ src/test/java/by/andd3dfx/collections/ContainsDuplicatesTest.java
@@ -3,29 +3,32 @@ package by.andd3dfx.collections;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import java.util.function.Function;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ContainsDuplicatesTest {
 
-    private ContainsDuplicates containsDuplicates;
+    private ContainsDuplicates solver;
 
     @Before
     public void setUp() throws Exception {
-        containsDuplicates = new ContainsDuplicates();
+        solver = new ContainsDuplicates();
     }
 
     @Test
     public void usingSet() {
-        assertTrue(containsDuplicates.usingSet(new int[]{2, 1, 3, 1}));
-        assertFalse(containsDuplicates.usingSet(new int[]{2, 1, 3, 56, 4}));
-        assertTrue(containsDuplicates.usingSet(new int[]{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}));
+        checkAssertions(solver::usingSet);
     }
 
     @Test
     public void usingSortWithEarlyReturn() {
-        assertTrue(containsDuplicates.usingSortWithEarlyReturn(new int[]{2, 1, 3, 1}));
-        assertFalse(containsDuplicates.usingSortWithEarlyReturn(new int[]{2, 1, 3, 56, 4}));
-        assertTrue(containsDuplicates.usingSortWithEarlyReturn(new int[]{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}));
+        checkAssertions(solver::usingSortWithEarlyReturn);
+    }
+
+    private void checkAssertions(Function<int[], Boolean> function) {
+        assertThat(function.apply(new int[]{2, 1, 3, 1})).isTrue();
+        assertThat(function.apply(new int[]{2, 1, 3, 56, 4})).isFalse();
+        assertThat(function.apply(new int[]{1, 1, 1, 3, 3, 4, 3, 2, 4, 2})).isTrue();
     }
 }
