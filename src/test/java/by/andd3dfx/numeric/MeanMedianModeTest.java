95d8a6594ba3a4fb8ed221212129b11441537728
Andrei Punko <andd3dfx@gmail.com>
2024-07-08 23:33:04 +0300

    Remove deprecated method usage

--- src/test/java/by/andd3dfx/numeric/MeanMedianModeTest.java
+++ src/test/java/by/andd3dfx/numeric/MeanMedianModeTest.java
@@ -8,48 +8,47 @@ import static by.andd3dfx.numeric.MeanMedianMode.mode;
 import static by.andd3dfx.numeric.MeanMedianMode.quartile1;
 import static by.andd3dfx.numeric.MeanMedianMode.quartile2;
 import static by.andd3dfx.numeric.MeanMedianMode.quartile3;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class MeanMedianModeTest {
 
     @Test
     public void testMean() {
-        assertThat(mean(new int[]{2, 2, 2, 2, 2}), is(2.0));
-        assertThat(mean(new int[]{1, 2, 3, 4}), is(2.5));
-        assertThat(mean(new int[]{1, 2, 3, 8}), is(3.5));
+        assertThat(mean(new int[]{2, 2, 2, 2, 2})).isEqualTo(2.0);
+        assertThat(mean(new int[]{1, 2, 3, 4})).isEqualTo(2.5);
+        assertThat(mean(new int[]{1, 2, 3, 8})).isEqualTo(3.5);
     }
 
     @Test
     public void testMedian() {
-        assertThat(median(new int[]{2, 2, 2, 2, 2}), is(2.0));
-        assertThat(median(new int[]{1, 2, 3, 4}), is(2.5));
-        assertThat(median(new int[]{1, 2, 3, 8}), is(2.5));
+        assertThat(median(new int[]{2, 2, 2, 2, 2})).isEqualTo(2.0);
+        assertThat(median(new int[]{1, 2, 3, 4})).isEqualTo(2.5);
+        assertThat(median(new int[]{1, 2, 3, 8})).isEqualTo(2.5);
     }
 
     @Test
     public void testMode() {
-        assertThat(mode(new int[]{1, 1, 1, 1}), is(1.0));
-        assertThat(mode(new int[]{1, 2, 3, 4}), is(1.0));
-        assertThat(mode(new int[]{1, 3, 3, 2, 5, 9}), is(3.0));
-        assertThat(mode(new int[]{1, 3, 3, 2, 5, 2, 9}), is(2.0));
+        assertThat(mode(new int[]{1, 1, 1, 1})).isEqualTo(1.0);
+        assertThat(mode(new int[]{1, 2, 3, 4})).isEqualTo(1.0);
+        assertThat(mode(new int[]{1, 3, 3, 2, 5, 9})).isEqualTo(3.0);
+        assertThat(mode(new int[]{1, 3, 3, 2, 5, 2, 9})).isEqualTo(2.0);
     }
 
     @Test
     public void testQuartile1() {
-        assertThat(quartile1(new int[]{1, 1, 1, 1}), is(1.0));
-        assertThat(quartile1(new int[]{5, 6, 7, 8, 1, 2, 3, 4}), is(2.5));
+        assertThat(quartile1(new int[]{1, 1, 1, 1})).isEqualTo(1.0);
+        assertThat(quartile1(new int[]{5, 6, 7, 8, 1, 2, 3, 4})).isEqualTo(2.5);
     }
 
     @Test
     public void testQuartile2() {
-        assertThat(quartile2(new int[]{1, 1, 1, 1}), is(1.0));
-        assertThat(quartile2(new int[]{5, 6, 7, 8, 1, 2, 3, 4}), is(4.5));
+        assertThat(quartile2(new int[]{1, 1, 1, 1})).isEqualTo(1.0);
+        assertThat(quartile2(new int[]{5, 6, 7, 8, 1, 2, 3, 4})).isEqualTo(4.5);
     }
 
     @Test
     public void testQuartile3() {
-        assertThat(quartile3(new int[]{1, 1, 1, 1}), is(1.0));
-        assertThat(quartile3(new int[]{5, 6, 7, 8, 1, 2, 3, 4}), is(6.5));
+        assertThat(quartile3(new int[]{1, 1, 1, 1})).isEqualTo(1.0);
+        assertThat(quartile3(new int[]{5, 6, 7, 8, 1, 2, 3, 4})).isEqualTo(6.5);
     }
 }
