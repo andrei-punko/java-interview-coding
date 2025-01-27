7ec67a47086adc88621785b599dfd3b6901b52ba
Andrei Punko <andd3dfx@gmail.com>
2025-01-27 11:56:01 +0300

    Add cases for even/odd length of loop in LinkedList task

--- src/test/java/by/andd3dfx/collections/LoopInLinkedListTest.java
+++ src/test/java/by/andd3dfx/collections/LoopInLinkedListTest.java
@@ -23,13 +23,29 @@ public class LoopInLinkedListTest {
     }
 
     @Test
-    public void findForListWithLoop() {
+    public void findForListWithOddLengthLoop() {
         Node node1 = new Node(10);
         Node node2 = new Node(11, node1);
         Node node3 = new Node(12, node2);
         Node node4 = new Node(13, node3);
         Node node5 = new Node(14, node4);
         node1.setNext(node3);
+
+        assertTrue(LoopInLinkedList.isPresent(node5));
+        assertTrue(LoopInLinkedList.isPresent(node4));
+        assertTrue(LoopInLinkedList.isPresent(node3));
+        assertTrue(LoopInLinkedList.isPresent(node2));
+        assertTrue(LoopInLinkedList.isPresent(node1));
+    }
+
+    @Test
+    public void findForListWithEvenLengthLoop() {
+        Node node1 = new Node(10);
+        Node node2 = new Node(11, node1);
+        Node node3 = new Node(12, node2);
+        Node node4 = new Node(13, node3);
+        Node node5 = new Node(14, node4);
+        node1.setNext(node4);
 
         assertTrue(LoopInLinkedList.isPresent(node5));
         assertTrue(LoopInLinkedList.isPresent(node4));
