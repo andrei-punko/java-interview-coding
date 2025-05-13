39b47ca6aeea7702423ba8f816da12da45b132e7
Andrei Punko <andd3dfx@gmail.com>
2025-05-13 20:57:29 +0300

    Optimize solution of InfectionDistribution task

--- src/main/java/by/andd3dfx/common/InfectionDistribution.java
+++ src/main/java/by/andd3dfx/common/InfectionDistribution.java
@@ -1,11 +1,7 @@
 package by.andd3dfx.common;
 
-import lombok.Data;
-
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -29,118 +25,41 @@ import java.util.stream.Collectors;
  */
 public class InfectionDistribution {
 
-    /**
-     * Initial solution
-     */
-    public static int weeksToInfectAllCities_usingIterationByRoads(int citiesAmount, int[][] roads, int[] infected) {
-        int weeksCount = 0;
-        var infectedCitiesSet = Arrays.stream(infected).boxed()
-                .collect(Collectors.toSet());
-        var roadsSet = buildRoadsSet(roads);
-
-        while (!isAllInfected(citiesAmount, infectedCitiesSet)) {
-            var infectedOnThisIteration = processOneWeek(roadsSet, infectedCitiesSet);
-
-            if (infectedOnThisIteration == 0) {
-                throw new IllegalArgumentException("Could not infect all cities!");
-            }
-            weeksCount++;
-        }
-
-        return weeksCount;
-    }
-
-    private static boolean isAllInfected(int citiesAmount, Set<Integer> infectedCitiesSet) {
-        return infectedCitiesSet.size() == citiesAmount;
-    }
-
-    private static Set<Road> buildRoadsSet(int[][] roads) {
-        return Arrays.stream(roads)
-                .map(Road::new)
-                .collect(Collectors.toSet());
-    }
-
-    private static int processOneWeek(Set<Road> roads, Set<Integer> infectedCitiesSet) {
-        var initialCitiesSet = new HashSet<>(infectedCitiesSet);
-
-        var it = roads.iterator();
-        while (it.hasNext()) {
-            var road = it.next();
-
-            if (initialCitiesSet.contains(road.start)) {
-                infectedCitiesSet.add(road.end);
-                it.remove();
-                continue;
-            }
-
-            if (initialCitiesSet.contains(road.end)) {
-                infectedCitiesSet.add(road.start);
-                it.remove();
-            }
-        }
-
-        return infectedCitiesSet.size() - initialCitiesSet.size();
-    }
-
-    @Data
-    private static class Road {
-        private int start;
-        private int end;
-
-        public Road(int[] road) {
-            this.start = road[0];
-            this.end = road[1];
-        }
-    }
-
-    /**
-     * Enhanced solution
-     */
-    public static int weeksToInfectAllCities_usingIterationByInfectedCities(int citiesAmount, int[][] roads, int[] infected) {
-        var routesMap = new HashMap<Integer, Set<Integer>>();
+    public static int weeksToInfectAllCities(int citiesAmount, int[][] roads, int[] infected) {
+        var adjMatrix = new boolean[citiesAmount][citiesAmount];    // adjacency matrix
         Arrays.stream(roads).forEach(road -> {
-            populate(routesMap, road, 0, 1);
-            populate(routesMap, road, 1, 0);
+            adjMatrix[road[0]][road[1]] = true;
+            adjMatrix[road[1]][road[0]] = true;
         });
 
         var weeksAmount = 0;
         var infectedCities = Arrays.stream(infected).boxed().collect(Collectors.toSet());
-        var sourceCities = new HashSet<>(infectedCities);
 
-        while (!isAllInfected(citiesAmount, infectedCities)) {
-            if (sourceCities.isEmpty()) {
-                throw new IllegalArgumentException("Could not infect all cities!");
+        while (infectedCities.size() < citiesAmount) {
+            var newCities = determineNewInfectedCities(infectedCities, adjMatrix);
+            if (newCities.isEmpty()) {
+                return -1;
             }
 
-            var newInfectedCities = determineNewInfectedCities(routesMap, infectedCities, sourceCities);
-            infectedCities.addAll(newInfectedCities);
-            sourceCities = newInfectedCities;
+            infectedCities.addAll(newCities);
+
             weeksAmount++;
         }
 
         return weeksAmount;
     }
 
-    private static HashSet<Integer> determineNewInfectedCities(
-            Map<Integer, Set<Integer>> routesMap, Set<Integer> infectedCities, Set<Integer> sourceCities) {
-        var newInfectedCities = new HashSet<Integer>();
-        for (var city : sourceCities) {
-            if (!routesMap.containsKey(city)) {
-                continue;
+    private static Set<Integer> determineNewInfectedCities(Set<Integer> infectedCities, boolean[][] adjMatrix) {
+        var n = adjMatrix.length;
+        var newCities = new HashSet<Integer>();
+        for (var city : infectedCities) {
+            for (int i = 0; i < n; i++) {
+                if (adjMatrix[city][i] && !infectedCities.contains(i)) {
+                    newCities.add(i);
+                }
             }
-
-            var citiesToInfect = new HashSet<>(routesMap.get(city));
-            citiesToInfect.removeAll(infectedCities);
-
-            newInfectedCities.addAll(citiesToInfect);
         }
-        return newInfectedCities;
-    }
 
-    private static void populate(Map<Integer, Set<Integer>> routesMap, int[] road, int i, int j) {
-        if (!routesMap.containsKey(road[i])) {
-            routesMap.put(road[i], new HashSet<>());
-        }
-        routesMap.get(road[i]).add(road[j]);
+        return newCities;
     }
 }
--- src/test/java/by/andd3dfx/common/InfectionDistributionTest.java
+++ src/test/java/by/andd3dfx/common/InfectionDistributionTest.java
@@ -2,28 +2,26 @@ package by.andd3dfx.common;
 
 import org.junit.Test;
 
-import static by.andd3dfx.common.InfectionDistribution.weeksToInfectAllCities_usingIterationByInfectedCities;
-import static by.andd3dfx.common.InfectionDistribution.weeksToInfectAllCities_usingIterationByRoads;
+import static by.andd3dfx.common.InfectionDistribution.weeksToInfectAllCities;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertThrows;
 
 public class InfectionDistributionTest {
 
     @Test
-    public void weeksToInfectAllCitiesPositive_usingIterationByRoads() {
-        assertThat(weeksToInfectAllCities_usingIterationByRoads(
+    public void weeksToInfectAllCitiesPositive() {
+        assertThat(weeksToInfectAllCities(
                 1,
                 new int[][]{},
                 new int[]{0}
         )).isEqualTo(0);
 
-        assertThat(weeksToInfectAllCities_usingIterationByRoads(
+        assertThat(weeksToInfectAllCities(
                 2,
                 new int[][]{new int[]{0, 1}},
                 new int[]{0}
         )).isEqualTo(1);
 
-        assertThat(weeksToInfectAllCities_usingIterationByRoads(
+        assertThat(weeksToInfectAllCities(
                 5,
                 new int[][]{
                         new int[]{0, 2},
@@ -33,7 +31,7 @@ public class InfectionDistributionTest {
                 new int[]{0, 3}
         )).isEqualTo(2);
 
-        assertThat(weeksToInfectAllCities_usingIterationByRoads(
+        assertThat(weeksToInfectAllCities(
                 7,
                 new int[][]{
                         new int[]{0, 2},
@@ -48,80 +46,20 @@ public class InfectionDistributionTest {
     }
 
     @Test
-    public void weeksToInfectAllCitiesNegative_usingIterationByRoads() {
-        assertThrows("Could not infect all cities!", IllegalArgumentException.class,
-                () -> weeksToInfectAllCities_usingIterationByRoads(
-                        1,
-                        new int[][]{},
-                        new int[]{}
-                ));
-
-        assertThrows("Could not infect all cities!", IllegalArgumentException.class,
-                () -> weeksToInfectAllCities_usingIterationByRoads(
-                        5,
-                        new int[][]{
-                                new int[]{0, 2},
-                                new int[]{1, 4},
-                        },
-                        new int[]{0, 3}
-                ));
-    }
-
-    @Test
-    public void weeksToInfectAllCitiesPositive_usingIterationByCities() {
-        assertThat(weeksToInfectAllCities_usingIterationByInfectedCities(
+    public void weeksToInfectAllCitiesNegative() {
+        assertThat(weeksToInfectAllCities(
                 1,
                 new int[][]{},
-                new int[]{0}
-        )).isEqualTo(0);
+                new int[]{}
+        )).isEqualTo(-1);
 
-        assertThat(weeksToInfectAllCities_usingIterationByInfectedCities(
-                2,
-                new int[][]{new int[]{0, 1}},
-                new int[]{0}
-        )).isEqualTo(1);
-
-        assertThat(weeksToInfectAllCities_usingIterationByInfectedCities(
+        assertThat(weeksToInfectAllCities(
                 5,
                 new int[][]{
                         new int[]{0, 2},
-                        new int[]{1, 2},
-                        new int[]{3, 4},
+                        new int[]{1, 4},
                 },
                 new int[]{0, 3}
-        )).isEqualTo(2);
-
-        assertThat(weeksToInfectAllCities_usingIterationByInfectedCities(
-                7,
-                new int[][]{
-                        new int[]{0, 2},
-                        new int[]{1, 2},
-                        new int[]{3, 4},
-                        new int[]{6, 4},
-                        new int[]{6, 5},
-                        new int[]{1, 5},
-                },
-                new int[]{0}
-        )).isEqualTo(6);
-    }
-
-    @Test
-    public void weeksToInfectAllCitiesNegative_usingIterationByCities() {
-        assertThrows("Could not infect all cities!", IllegalArgumentException.class,
-                () -> weeksToInfectAllCities_usingIterationByInfectedCities(
-                        1,
-                        new int[][]{},
-                        new int[]{}
-                ));
-
-        assertThrows("Could not infect all cities!", IllegalArgumentException.class,
-                () -> weeksToInfectAllCities_usingIterationByInfectedCities(
-                        5,
-                        new int[][]{
-                                new int[]{0, 2},
-                                new int[]{1, 4},
-                        },
-                        new int[]{0, 3}
-                ));
+        )).isEqualTo(-1);
     }
 }
