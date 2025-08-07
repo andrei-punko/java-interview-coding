dfd79e495f1e2e959d17a6030638588df60bc585
Andrei Punko <andd3dfx@gmail.com>
2025-08-07 18:13:30 +0300

    Add FilterUsers task solution

---
+++ src/main/java/by/andd3dfx/collections/FilterUsers.java
@@ -0,0 +1,46 @@
+package by.andd3dfx.collections;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * <pre>
+ * Необходимо создать стрим, который отфильтрует пользователей,
+ * у которых дата последнего логина была не позже, чем 3 дня назад,
+ * отобрать только уникальные имена, сгруппировать по количеству букв в имени
+ * и вывести список имен с наибольшим количеством букв
+ *
+ * static class User {
+ *     private String name;
+ *     private LocalDate lastLogin;
+ * }
+ * </pre>
+ */
+public class FilterUsers {
+
+    public static List<String> apply(List<User> users) {
+        return users.stream()
+                .filter(user -> !user.lastLogin
+                        .plusDays(3)
+                        .isBefore(LocalDate.now()))
+                .map(User::getName)
+                .distinct()
+                .collect(Collectors.groupingBy(String::length)).entrySet().stream()
+                .max(Comparator.comparingInt(Map.Entry::getKey))
+                .map(Map.Entry::getValue)
+                .orElseGet(List::of);
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class User {
+        private String name;
+        private LocalDate lastLogin;
+    }
+}
---
+++ src/test/java/by/andd3dfx/collections/FilterUsersTest.java
@@ -0,0 +1,36 @@
+package by.andd3dfx.collections;
+
+import by.andd3dfx.collections.FilterUsers.User;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FilterUsersTest {
+
+    @Test
+    public void apply() {
+        List<User> users = List.of(
+                new User("Anton", LocalDate.now().minusDays(5)),
+                new User("Gena", LocalDate.now().minusDays(3)),
+                new User("John", LocalDate.now().minusDays(2)),
+                new User("Vladimir", LocalDate.now().minusDays(1)),
+                new User("Anatoliy", LocalDate.now().minusDays(3))
+        );
+
+        var result = FilterUsers.apply(users);
+
+        assertThat(result).isEqualTo(List.of("Vladimir", "Anatoliy"));
+    }
+
+    @Test
+    public void applyForEmpty() {
+        List<User> users = List.of();
+
+        var result = FilterUsers.apply(users);
+
+        assertThat(result).isEmpty();
+    }
+}
