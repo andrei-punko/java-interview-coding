ab466869986e9a92cde57f9ee2411d8ca05531b9
Andrei Punko <andd3dfx@gmail.com>
2026-01-15 23:53:45 +0300

    Add task definition for ReverseLinkedList task

--- src/main/java/by/andd3dfx/collections/ReverseLinkedList.java
+++ src/main/java/by/andd3dfx/collections/ReverseLinkedList.java
@@ -1,12 +1,31 @@
 package by.andd3dfx.collections;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-
 /**
+ * <pre>
+ * <a href="https://leetcode.com/problems/reverse-linked-list/">Task description</a>
+ *
+ * Given the head of a singly linked list, reverse the list, and return the reversed list.
+ *
+ * Example 1:
+ * <img src="https://assets.leetcode.com/uploads/2021/02/19/rev1ex1.jpg"/>
+ * Input: head = [1,2,3,4,5]
+ * Output: [5,4,3,2,1]
+ *
+ * Example 2:
+ * <img src="https://assets.leetcode.com/uploads/2021/02/19/rev1ex2.jpg"/>
+ * Input: head = [1,2]
+ * Output: [2,1]
+ *
+ * Example 3:
+ * Input: head = []
+ * Output: []
+ * </pre>
+ *
  * @see <a href="https://youtu.be/iEKdRgKNurg">Video solution</a>
  */
 public class ReverseLinkedList {
@@ -14,6 +33,7 @@ public class ReverseLinkedList {
     @Data
     @AllArgsConstructor
     public static class Node<T> {
+
         private T value;
         private Node next;
 
