1043872519000c07a1d92b77aaa452e377858b40
Andrei Punko <andd3dfx@gmail.com>
2026-01-15 22:55:48 +0300

    Add MissingNumber task solution

---
+++ src/test/java/by/andd3dfx/numeric/MissingNumberTest.java
@@ -0,0 +1,17 @@
+package by.andd3dfx.numeric;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class MissingNumberTest {
+
+    private final MissingNumber missingNumber = new MissingNumber();
+
+    @Test
+    public void testMissingNumber() {
+        assertThat(missingNumber.missingNumber(new int[]{3, 0, 1})).isEqualTo(2);
+        assertThat(missingNumber.missingNumber(new int[]{0, 1})).isEqualTo(2);
+        assertThat(missingNumber.missingNumber(new int[]{9, 6, 4, 2, 3, 5, 7, 0, 1})).isEqualTo(8);
+    }
+}
