ad2abfc863c450e9355596ed4a8167844c25622e
Andrei Punko <andd3dfx@gmail.com>
2026-01-16 03:20:49 +0300

    Remove duplicate solution of SumOfTwo task

--- src/test/java/by/andd3dfx/search/FindSumOfTwoInArrayTest.java
+++ src/test/java/by/andd3dfx/search/FindSumOfTwoInArrayTest.java
@@ -1,8 +1,8 @@
 package by.andd3dfx.search;
 
-import org.junit.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
 
 public class FindSumOfTwoInArrayTest {
 
@@ -24,5 +24,65 @@ public class FindSumOfTwoInArrayTest {
             .containsExactlyInAnyOrder(0, 3);
         assertThat(FindSumOfTwoInArray.find_On(new int[]{1, 4, 7, 2, 90}, 19))
             .isNull();
+    }
+
+    /// Cases from deleted implementation copy
+
+    @Test
+    public void testFind_ON() {
+        var result = FindSumOfTwoInArray.find_On(new int[]{1, 3, 5, 7, 9}, 12);
+
+        assertThat(result).satisfiesAnyOf(
+            items -> assertThat(items).isEqualTo(new int[]{1, 4}),
+            items -> assertThat(items).isEqualTo(new int[]{2, 3}),
+            items -> assertThat(items).isEqualTo(new int[]{3, 2}),
+            items -> assertThat(items).isEqualTo(new int[]{4, 1})
+        );
+    }
+
+    @Test
+    public void testFind_ON2() {
+        var result = FindSumOfTwoInArray.find_On2(new int[]{1, 3, 5, 7, 9}, 12);
+
+        assertThat(result).satisfiesAnyOf(
+            items -> assertThat(items).isEqualTo(new int[]{1, 4}),
+            items -> assertThat(items).isEqualTo(new int[]{2, 3}),
+            items -> assertThat(items).isEqualTo(new int[]{3, 2}),
+            items -> assertThat(items).isEqualTo(new int[]{4, 1})
+        );
+    }
+
+    @Test
+    public void testFind_ONForSameNumbers() {
+        var result = FindSumOfTwoInArray.find_On(new int[]{1, 1, 6, 6, 1}, 12);
+
+        assertThat(result).satisfiesAnyOf(
+            items -> assertThat(items).isEqualTo(new int[]{2, 3}),
+            items -> assertThat(items).isEqualTo(new int[]{3, 2})
+        );
+    }
+
+    @Test
+    public void testFind_ON2ForSameNumbers() {
+        var result = FindSumOfTwoInArray.find_On2(new int[]{1, 1, 6, 6, 1}, 12);
+
+        assertThat(result).satisfiesAnyOf(
+            items -> assertThat(items).isEqualTo(new int[]{2, 3}),
+            items -> assertThat(items).isEqualTo(new int[]{3, 2})
+        );
+    }
+
+    @Test
+    public void testFind_ONForAbsentSolution() {
+        var result = FindSumOfTwoInArray.find_On(new int[]{1, 1, 6, 3, 1}, 12);
+
+        assertThat(result).isNull();
+    }
+
+    @Test
+    public void testFind_ON2ForAbsentSolution() {
+        var result = FindSumOfTwoInArray.find_On2(new int[]{1, 1, 6, 3, 1}, 12);
+
+        assertThat(result).isNull();
     }
 }
