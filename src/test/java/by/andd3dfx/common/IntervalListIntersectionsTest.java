0f7067ff833122c172014fced8888e56e43c8d8d
Andrei Punko <andd3dfx@gmail.com>
2026-02-06 23:39:20 +0300

    IntervalListIntersections: reorder test-cases

--- src/test/java/by/andd3dfx/common/IntervalListIntersectionsTest.java
+++ src/test/java/by/andd3dfx/common/IntervalListIntersectionsTest.java
@@ -1,6 +1,5 @@
 package by.andd3dfx.common;
 
-import static by.andd3dfx.common.IntervalListIntersections.intervalIntersection;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
@@ -9,14 +8,14 @@ public class IntervalListIntersectionsTest {
 
     @Test
     public void testIntervalIntersection() {
-        assertThat(intervalIntersection(
+        assertThat(IntervalListIntersections.intervalIntersection(
+            new int[][]{{1, 3}, {5, 9}},
+            new int[][]{}
+        )).isEqualTo(new int[][]{});
+
+        assertThat(IntervalListIntersections.intervalIntersection(
             new int[][]{{0, 2}, {5, 10}, {13, 23}, {24, 25}},
             new int[][]{{1, 5}, {8, 12}, {15, 24}, {25, 26}}
         )).isEqualTo(new int[][]{{1, 2}, {5, 5}, {8, 10}, {15, 23}, {24, 24}, {25, 25}});
-
-        assertThat(intervalIntersection(
-            new int[][]{{1, 3}, {5, 9}},
-            new int[][]{}))
-            .isEqualTo(new int[][]{});
     }
 }
